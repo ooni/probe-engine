79338ce6ff54e6497c9c4dfcb79d6f726269bcfc
Simone Basso <bassosimone@gmail.com>
2019-05-21 12:28:34 +0200

    log/log.go: improve documentation

--- log/log.go
+++ log/log.go
@@ -1,7 +1,12 @@
-// Package log implements logging.
+// Package log defines the logging interface used in this library. This
+// logging interface is compatible with github.com/apex/log. However, we
+// only use the interface in this library. Therefore, it's possible in
+// principle for you to use this library with another logger, as long as
+// you make such logger implement this interface.
 package log
 
-// Logger emits log messages.
+// Logger defines the common interface that a logger should have. It is
+// out of the box compatible with `log.Log` in `apex/log`.
 type Logger interface {
 	// Debug emits a debug message.
 	Debug(msg string)
