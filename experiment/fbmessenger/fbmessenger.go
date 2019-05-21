465a1b6d7da6025123cac52075f472d0936350ea
Simone Basso <bassosimone@gmail.com>
2019-05-21 22:36:49 +0000

    Implement facebook_messenger MK wrapper

---
+++ experiment/fbmessenger/fbmessenger.go
@@ -0,0 +1,46 @@
+// Package fbmessenger contains the Facebook Messenger network experiment.
+package fbmessenger
+
+import (
+	"context"
+
+	"github.com/ooni/probe-engine/experiment"
+	"github.com/ooni/probe-engine/experiment/mkevent"
+	"github.com/ooni/probe-engine/measurementkit"
+	"github.com/ooni/probe-engine/model"
+	"github.com/ooni/probe-engine/session"
+)
+
+const (
+	testName    = "facebook_messenger"
+	testVersion = "0.0.2"
+)
+
+// Config contains the experiment config.
+type Config struct{}
+
+func measure(
+	ctx context.Context, sess *session.Session, measurement *model.Measurement,
+) error {
+	settings := measurementkit.NewSettings(
+		"FacebookMessenger", sess.SoftwareName, sess.SoftwareVersion,
+		sess.CABundlePath(), sess.ProbeASNString(), sess.ProbeCC(),
+		sess.ProbeIP(), sess.ProbeNetworkName(),
+	)
+	settings.Options.GeoIPASNPath = sess.ASNDatabasePath()
+	out, err := measurementkit.StartEx(settings, sess.Logger)
+	if err != nil {
+		return err
+	}
+	for ev := range out {
+		mkevent.Handle(sess, measurement, ev)
+	}
+	return nil
+}
+
+// NewExperiment creates a new experiment.
+func NewExperiment(
+	sess *session.Session, config Config,
+) *experiment.Experiment {
+	return experiment.New(sess, testName, testVersion, measure)
+}
---
+++ experiment/fbmessenger/fbmessenger_test.go
@@ -0,0 +1,43 @@
+package fbmessenger_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/apex/log"
+	"github.com/ooni/probe-engine/experiment/fbmessenger"
+	"github.com/ooni/probe-engine/session"
+)
+
+const (
+	softwareName    = "ooniprobe-example"
+	softwareVersion = "0.0.1"
+)
+
+func TestIntegration(t *testing.T) {
+	log.SetLevel(log.DebugLevel)
+	ctx := context.Background()
+
+	sess := session.New(log.Log, softwareName, softwareVersion)
+	sess.WorkDir = "../../testdata"
+	if err := sess.LookupBackends(ctx); err != nil {
+		t.Fatal(err)
+	}
+	if err := sess.LookupLocation(ctx); err != nil {
+		t.Fatal(err)
+	}
+
+	experiment := fbmessenger.NewExperiment(sess, fbmessenger.Config{})
+	if err := experiment.OpenReport(ctx); err != nil {
+		t.Fatal(err)
+	}
+	defer experiment.CloseReport(ctx)
+
+	measurement, err := experiment.Measure(ctx, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := experiment.SubmitMeasurement(ctx, &measurement); err != nil {
+		t.Fatal(err)
+	}
+}
