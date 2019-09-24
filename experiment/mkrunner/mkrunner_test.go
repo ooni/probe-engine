2207facfa0128780f3cab68d7f2327dc01f4bc3c
Simone Basso <bassosimone@gmail.com>
2019-09-24 11:14:45 +0200

    Increase coverage of MK based experiments
    While there, add a basic runtime check that gives us more confidence
    that we did actually correctly run a measurement.

---
+++ experiment/mkrunner/mkrunner_test.go
@@ -0,0 +1,44 @@
+package mkrunner_test
+
+import (
+	"testing"
+
+	apexlog "github.com/apex/log"
+	"github.com/ooni/probe-engine/experiment/handler"
+	"github.com/ooni/probe-engine/experiment/mkrunner"
+	"github.com/ooni/probe-engine/measurementkit"
+	"github.com/ooni/probe-engine/model"
+	"github.com/ooni/probe-engine/session"
+)
+
+func TestIntegrationSuccess(t *testing.T) {
+	err := mkrunner.Do(
+		measurementkit.Settings{},
+		session.New(
+			apexlog.Log, "ooniprobe-engine", "0.1.0",
+			"../../testdata", nil, nil,
+		),
+		&model.Measurement{},
+		handler.NewPrinterCallbacks(apexlog.Log),
+		mkrunner.DoNothingStartEx,
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestIntegrationFailure(t *testing.T) {
+	err := mkrunner.Do(
+		measurementkit.Settings{},
+		session.New(
+			apexlog.Log, "ooniprobe-engine", "0.1.0",
+			"../../testdata", nil, nil,
+		),
+		&model.Measurement{},
+		handler.NewPrinterCallbacks(apexlog.Log),
+		mkrunner.FailingStartEx,
+	)
+	if err == nil {
+		t.Fatal("expected an error here")
+	}
+}
