28a29169ffb97fc6ceb008579175a44f9d2ea007
Simone Basso <bassosimone@gmail.com>
2019-10-11 10:28:56 +0200

    sniblocking.go: increase code coverage

--- experiment/sniblocking/sniblocking_test.go
+++ experiment/sniblocking/sniblocking_test.go
@@ -1,12 +1,12 @@
-package sniblocking_test
+package sniblocking
 
 import (
 	"context"
 	"testing"
 
 	"github.com/apex/log"
-	"github.com/ooni/probe-engine/experiment/sniblocking"
-	"github.com/ooni/probe-engine/measurementkit"
+	"github.com/ooni/probe-engine/experiment/handler"
+	"github.com/ooni/probe-engine/model"
 	"github.com/ooni/probe-engine/session"
 )
 
@@ -16,9 +16,6 @@ const (
 )
 
 func TestIntegration(t *testing.T) {
-	if !measurementkit.Available() {
-		t.Skip("Measurement Kit not available; skipping")
-	}
 	log.SetLevel(log.DebugLevel)
 	ctx := context.Background()
 
@@ -32,7 +29,7 @@ func TestIntegration(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	experiment := sniblocking.NewExperiment(sess, sniblocking.Config{})
+	experiment := NewExperiment(sess, Config{})
 	if err := experiment.OpenReport(ctx); err != nil {
 		t.Fatal(err)
 	}
@@ -42,7 +39,38 @@ func TestIntegration(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	tk := measurement.TestKeys.(*TestKeys)
+	if tk.FailureWithProperSNI != nil {
+		t.Fatal("expected no failure here")
+	}
+	if tk.FailureWithRandomSNI == nil {
+		t.Fatal("expected a failure here")
+	}
+
 	if err := experiment.SubmitMeasurement(ctx, &measurement); err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestFailureProperSNI(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel() // immediately fail it
+	sess := session.New(
+		log.Log, softwareName, softwareVersion, "../../testdata", nil, nil,
+	)
+	var measurement model.Measurement
+	var config Config
+	callbacks := handler.NewPrinterCallbacks(log.Log)
+	err := measure(ctx, sess, &measurement, callbacks, config)
+	if err != nil {
+		t.Fatal("expected no error here")
+	}
+	tk := measurement.TestKeys.(*TestKeys)
+	if tk.FailureWithProperSNI == nil {
+		t.Fatal("expected a failure here")
+	}
+	if tk.FailureWithRandomSNI == nil {
+		t.Fatal("expected a failure here")
 	}
 }
