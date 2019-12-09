f8670d057ab1056f1fb0d365d6254fd99e415289
Simone Basso <bassosimone@gmail.com>
2019-12-09 09:52:33 +0100

    orchestra: implement metadata validation (#161)
    Follows closely the algorithm implemented by MK:
    
    https://github.com/measurement-kit/measurement-kit/blob/ba98a20731a747ffb32e19991824c56a32f763a2/src/libmeasurement_kit/ooni/orchestrate_impl.hpp#L100
    
    This is part of #148

--- internal/orchestra/metadata/metadata.go
+++ internal/orchestra/metadata/metadata.go
@@ -17,3 +17,37 @@ type Metadata struct {
 	SoftwareVersion    string   `json:"software_version"`
 	SupportedTests     []string `json:"supported_tests"`
 }
+
+// Valid returns true if metadata is valid, false otherwise. Metadata is
+// considered valid if all the mandatory fields are not empty. If a field
+// is marked `json:",omitempty"` in the structure definition, then it's
+// for sure mandatory. The "device_token" field is mandatory only if the
+// platform is "ios" or "android", because there's currently no device
+// token that we know of for desktop devices.
+func (m *Metadata) Valid() bool {
+	if m.ProbeCC == "" {
+		return false
+	}
+	if m.ProbeASN == "" {
+		return false
+	}
+	if m.Platform == "" {
+		return false
+	}
+	if m.SoftwareName == "" {
+		return false
+	}
+	if m.SoftwareVersion == "" {
+		return false
+	}
+	if len(m.SupportedTests) < 1 {
+		return false
+	}
+	switch m.Platform {
+	case "ios", "android":
+		if m.DeviceToken == "" {
+			return false
+		}
+	}
+	return true
+}
