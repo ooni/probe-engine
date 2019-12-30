1dc397f4a6ffb38fde773da57fea156b4167fa9c
Simone Basso <bassosimone@gmail.com>
2019-12-30 12:19:32 +0100

    http: save headers as a list
    Closes #114

--- experiment/oodatamodel/oodatamodel_test.go
+++ experiment/oodatamodel/oodatamodel_test.go
@@ -215,6 +215,24 @@ func TestUnitNewRequestsListGood(t *testing.T) {
 	if out[1].Request.Headers["Content-Length"].Value != "17" {
 		t.Fatal("unexpected out[1].Request.Headers Content-Length value")
 	}
+	if out[1].Request.HeadersList[0].Key != "Content-Type" {
+		t.Fatal("unexpected out[1].Request.HeadersList[0].Key value")
+	}
+	if out[1].Request.HeadersList[0].Value.Value != "text/plain" {
+		t.Fatal("unexpected out[1].Request.HeadersList[0].Value.Value value")
+	}
+	if out[1].Request.HeadersList[1].Key != "Content-Type" {
+		t.Fatal("unexpected out[1].Request.HeadersList[1].Key value")
+	}
+	if out[1].Request.HeadersList[1].Value.Value != "foobar" {
+		t.Fatal("unexpected out[1].Request.HeadersList[1].Value.Value value")
+	}
+	if out[1].Request.HeadersList[2].Key != "Content-Length" {
+		t.Fatal("unexpected out[1].Request.HeadersList[2].Key value")
+	}
+	if out[1].Request.HeadersList[2].Value.Value != "17" {
+		t.Fatal("unexpected out[1].Request.HeadersList[2].Value.Value value")
+	}
 	if out[1].Request.Method != "GET" {
 		t.Fatal("unexpected out[1].Request.Method")
 	}
@@ -241,6 +259,30 @@ func TestUnitNewRequestsListGood(t *testing.T) {
 	}
 	if out[1].Response.Headers["Content-Length"].Value != "14" {
 		t.Fatal("unexpected out[1].Response.Headers Content-Length value")
+	}
+	if out[1].Response.HeadersList[0].Key != "Content-Type" {
+		t.Fatal("unexpected out[1].Response.HeadersList[0].Key value")
+	}
+	if out[1].Response.HeadersList[0].Value.Value != "application/json" {
+		t.Fatal("unexpected out[1].Response.HeadersList[0].Value.Value value")
+	}
+	if out[1].Response.HeadersList[1].Key != "Content-Type" {
+		t.Fatal("unexpected out[1].Response.HeadersList[1].Key value")
+	}
+	if out[1].Response.HeadersList[1].Value.Value != "foobaz" {
+		t.Fatal("unexpected out[1].Response.HeadersList[1].Value.Value value")
+	}
+	if out[1].Response.HeadersList[2].Key != "Server" {
+		t.Fatal("unexpected out[1].Response.HeadersList[2].Key value")
+	}
+	if out[1].Response.HeadersList[2].Value.Value != "antani" {
+		t.Fatal("unexpected out[1].Response.HeadersList[2].Value.Value value")
+	}
+	if out[1].Response.HeadersList[3].Key != "Content-Length" {
+		t.Fatal("unexpected out[1].Response.HeadersList[3].Key value")
+	}
+	if out[1].Response.HeadersList[3].Value.Value != "14" {
+		t.Fatal("unexpected out[1].Response.HeadersList[3].Value.Value value")
 	}
 	if out[1].Response.BodyIsTruncated != false {
 		t.Fatal("unexpected out[1].Response.BodyIsTruncated")
@@ -283,28 +325,96 @@ func TestMarshalHTTPBodyString(t *testing.T) {
 	}
 }
 
+var binaryInput = []uint8{
+	0x57, 0xe5, 0x79, 0xfb, 0xa6, 0xbb, 0x0d, 0xbc, 0xce, 0xbd, 0xa7, 0xa0,
+	0xba, 0xa4, 0x78, 0x78, 0x12, 0x59, 0xee, 0x68, 0x39, 0xa4, 0x07, 0x98,
+	0xc5, 0x3e, 0xbc, 0x55, 0xcb, 0xfe, 0x34, 0x3c, 0x7e, 0x1b, 0x5a, 0xb3,
+	0x22, 0x9d, 0xc1, 0x2d, 0x6e, 0xca, 0x5b, 0xf1, 0x10, 0x25, 0x47, 0x1e,
+	0x44, 0xe2, 0x2d, 0x60, 0x08, 0xea, 0xb0, 0x0a, 0xcc, 0x05, 0x48, 0xa0,
+	0xf5, 0x78, 0x38, 0xf0, 0xdb, 0x3f, 0x9d, 0x9f, 0x25, 0x6f, 0x89, 0x00,
+	0x96, 0x93, 0xaf, 0x43, 0xac, 0x4d, 0xc9, 0xac, 0x13, 0xdb, 0x22, 0xbe,
+	0x7a, 0x7d, 0xd9, 0x24, 0xa2, 0x52, 0x69, 0xd8, 0x89, 0xc1, 0xd1, 0x57,
+	0xaa, 0x04, 0x2b, 0xa2, 0xd8, 0xb1, 0x19, 0xf6, 0xd5, 0x11, 0x39, 0xbb,
+	0x80, 0xcf, 0x86, 0xf9, 0x5f, 0x9d, 0x8c, 0xab, 0xf5, 0xc5, 0x74, 0x24,
+	0x3a, 0xa2, 0xd4, 0x40, 0x4e, 0xd7, 0x10, 0x1f,
+}
+
 func TestMarshalHTTPBodyBinary(t *testing.T) {
-	input := []uint8{
-		0x57, 0xe5, 0x79, 0xfb, 0xa6, 0xbb, 0x0d, 0xbc, 0xce, 0xbd, 0xa7, 0xa0,
-		0xba, 0xa4, 0x78, 0x78, 0x12, 0x59, 0xee, 0x68, 0x39, 0xa4, 0x07, 0x98,
-		0xc5, 0x3e, 0xbc, 0x55, 0xcb, 0xfe, 0x34, 0x3c, 0x7e, 0x1b, 0x5a, 0xb3,
-		0x22, 0x9d, 0xc1, 0x2d, 0x6e, 0xca, 0x5b, 0xf1, 0x10, 0x25, 0x47, 0x1e,
-		0x44, 0xe2, 0x2d, 0x60, 0x08, 0xea, 0xb0, 0x0a, 0xcc, 0x05, 0x48, 0xa0,
-		0xf5, 0x78, 0x38, 0xf0, 0xdb, 0x3f, 0x9d, 0x9f, 0x25, 0x6f, 0x89, 0x00,
-		0x96, 0x93, 0xaf, 0x43, 0xac, 0x4d, 0xc9, 0xac, 0x13, 0xdb, 0x22, 0xbe,
-		0x7a, 0x7d, 0xd9, 0x24, 0xa2, 0x52, 0x69, 0xd8, 0x89, 0xc1, 0xd1, 0x57,
-		0xaa, 0x04, 0x2b, 0xa2, 0xd8, 0xb1, 0x19, 0xf6, 0xd5, 0x11, 0x39, 0xbb,
-		0x80, 0xcf, 0x86, 0xf9, 0x5f, 0x9d, 0x8c, 0xab, 0xf5, 0xc5, 0x74, 0x24,
-		0x3a, 0xa2, 0xd4, 0x40, 0x4e, 0xd7, 0x10, 0x1f,
-	}
 	mbv := oodatamodel.HTTPBody{
-		Value: string(input),
+		Value: string(binaryInput),
 	}
 	data, err := json.Marshal(mbv)
 	if err != nil {
 		t.Fatal(err)
 	}
 	if !bytes.Equal(data, []byte(`{"data":"V+V5+6a7DbzOvaeguqR4eBJZ7mg5pAeYxT68Vcv+NDx+G1qzIp3BLW7KW/EQJUceROItYAjqsArMBUig9Xg48Ns/nZ8lb4kAlpOvQ6xNyawT2yK+en3ZJKJSadiJwdFXqgQrotixGfbVETm7gM+G+V+djKv1xXQkOqLUQE7XEB8=","format":"base64"}`)) {
+		t.Fatal("result is unexpected")
+	}
+}
+
+func TestMarshalHTTPHeaderString(t *testing.T) {
+	mbh := oodatamodel.HTTPHeadersList{
+		oodatamodel.HTTPHeader{
+			Key: "Content-Type",
+			Value: oodatamodel.MaybeBinaryValue{
+				Value: "application/json",
+			},
+		},
+		oodatamodel.HTTPHeader{
+			Key: "Content-Type",
+			Value: oodatamodel.MaybeBinaryValue{
+				Value: "antani",
+			},
+		},
+		oodatamodel.HTTPHeader{
+			Key: "Content-Length",
+			Value: oodatamodel.MaybeBinaryValue{
+				Value: "17",
+			},
+		},
+	}
+	data, err := json.Marshal(mbh)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := []byte(
+		`[["Content-Type","application/json"],["Content-Type","antani"],["Content-Length","17"]]`,
+	)
+	if !bytes.Equal(data, expected) {
+		t.Fatal("result is unexpected")
+	}
+}
+
+func TestMarshalHTTPHeaderBinary(t *testing.T) {
+	mbh := oodatamodel.HTTPHeadersList{
+		oodatamodel.HTTPHeader{
+			Key: "Content-Type",
+			Value: oodatamodel.MaybeBinaryValue{
+				Value: "application/json",
+			},
+		},
+		oodatamodel.HTTPHeader{
+			Key: "Content-Type",
+			Value: oodatamodel.MaybeBinaryValue{
+				Value: string(binaryInput),
+			},
+		},
+		oodatamodel.HTTPHeader{
+			Key: "Content-Length",
+			Value: oodatamodel.MaybeBinaryValue{
+				Value: "17",
+			},
+		},
+	}
+	data, err := json.Marshal(mbh)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(string(data))
+	expected := []byte(
+		`[["Content-Type","application/json"],["Content-Type",{"data":"V+V5+6a7DbzOvaeguqR4eBJZ7mg5pAeYxT68Vcv+NDx+G1qzIp3BLW7KW/EQJUceROItYAjqsArMBUig9Xg48Ns/nZ8lb4kAlpOvQ6xNyawT2yK+en3ZJKJSadiJwdFXqgQrotixGfbVETm7gM+G+V+djKv1xXQkOqLUQE7XEB8=","format":"base64"}],["Content-Length","17"]]`,
+	)
+	if !bytes.Equal(data, expected) {
 		t.Fatal("result is unexpected")
 	}
 }
