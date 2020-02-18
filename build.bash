c737b683ea9f44c3f758d3b28f8d86be96bc013c
Simone Basso <bassosimone@gmail.com>
2020-02-18 13:54:55 +0100

    libooni: add build support for Windows

--- build.bash
+++ build.bash
@@ -4,28 +4,46 @@ set -e
 topdir=$(cd $(dirname $0) && pwd -P)
 cd $topdir
 
-function library() {
-    if [ $# -ne 2 ]; then
-        echo "usage: library <goos> <goarch>" 1>&2
-        exit 1
-    fi
-    local goos=$1
-    local goarch=$2
-    local distbasedir=./dist/$goos/$goarch
-    rm -rf $distbasedir
-    local libdir=$distbasedir/lib
-    mkdir -p $libdir
-    local includedir=$distbasedir/include/ooni
-    mkdir -p $includedir
-    local library=$libdir/libooni.so
-    local buildmode=c-shared
-    echo "compile $goos/$goarch"
-    set -x
-    cp ./libooni/ffi.h $includedir
-    CGO_ENABLED=1 GOOS=$goos GOARCH=$goarch go build -v -o $library \
-	    -tags nomk -buildmode=$buildmode ./libooni/...
-    set +x
+function verbose() {
+  echo "+ $@"
+  "$@"
 }
 
-library linux amd64
-library windows amd64
+function build_libooni() {
+  if [ -z $GOOS -o -z $GOARCH ]; then
+    echo "fatal: build_libooni requires GOOS and GOARCH to be set" 1>&2
+    exit 1
+  fi
+  if [ $# -ne 3 ]; then
+    echo "usage: build_libooni <destdir> <buildmode> <libooni_name>" 1>&2
+    exit 1
+  fi
+  local destdir=$1
+  local includedir=$destdir/include/ooni
+  verbose rm -rf $includedir
+  verbose install -d $includedir
+  local libdir=$destdir/lib
+  verbose rm -rf $libdir
+  verbose install -d libdir
+  local buildmode=$2
+  local libooni_name=$3
+  local output=$libdir/$libooni_name
+  verbose cp ./libooni/ffi.h $includedir
+  verbose go build -v -tags nomk -ldflags="-s -w" -buildmode=$buildmode -o $output ./libooni
+  verbose rm -f $libdir/libooni.h
+}
+
+if [ "$1" = "macos" ]; then
+  verbose export GOOS=darwin
+  verbose export GOARCH=amd64
+  build_libooni dist/macos/amd64 c-shared libooni.dylib
+elif [ "$1" = "windows" ]; then
+  verbose export CC=x86_64-w64-mingw32-gcc
+  verbose export CGO_ENABLED=1
+  verbose export GOOS=windows
+  verbose export GOARCH=amd64
+  build_libooni dist/windows/amd64 c-shared libooni.dll
+else
+    echo "usage: $0 macos" 1>&2
+    exit 1
+fi
