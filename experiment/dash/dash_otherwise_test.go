569c24eaa69a03b683606dcce54c87db6d43cce8
Simone Basso <bassosimone@gmail.com>
2020-03-04 19:40:34 +0100

    Fix more tests

--- experiment/dash/dash_otherwise_test.go
+++ experiment/dash/dash_otherwise_test.go
@@ -25,7 +25,7 @@ const (
 func TestUnitNewExperiment(t *testing.T) {
 	sess := session.New(
 		log.Log, softwareName, softwareVersion,
-		"../../testdata", nil, nil, "../../testdata",
+		"../../testdata", nil, "../../testdata",
 		kvstore.NewMemoryKeyValueStore(),
 	)
 	experiment := NewExperiment(sess, Config{})
