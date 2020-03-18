99af5f3ec831db9c9ac335973cb81c77da58f863
Simone Basso <bassosimone@gmail.com>
2020-03-18 14:54:48 +0100

    Now ndt is an alias for ndt7, introduce ndt5
    Since now, if you request for ndt, we will give you ndt7 rather than
    ndt5. You can still get ndt5 if you explicitly ask for that.
    
    Significantly bump the version number used by ndt7 such that we leave
    some room to grow MK's ndt5 implementation, just in case.
    
    Part of https://github.com/ooni/backend/issues/369.

--- experiment/ndt5/ndt5_test.go
+++ experiment/ndt5/ndt5_test.go
@@ -1,16 +1,16 @@
-package ndt_test
+package ndt5_test
 
 import (
 	"testing"
 
 	"github.com/ooni/probe-engine/experiment/mktesting"
-	"github.com/ooni/probe-engine/experiment/ndt"
+	"github.com/ooni/probe-engine/experiment/ndt5"
 	"github.com/ooni/probe-engine/model"
 )
 
 func TestIntegration(t *testing.T) {
 	err := mktesting.Run("", func() model.ExperimentMeasurer {
-		return ndt.NewExperimentMeasurer(ndt.Config{})
+		return ndt5.NewExperimentMeasurer(ndt5.Config{})
 	})
 	if err != nil {
 		t.Fatal(err)
