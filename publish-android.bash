d690b1b89d33d8584b27fd58fd57940058531f90
Simone Basso <bassosimone@gmail.com>
2020-03-30 19:36:58 +0200

    Automatically publish packages using bintray
    Includes instructions regarding how to delete old packages.
    
    Closes https://github.com/ooni/probe-engine/issues/376.

---
+++ publish-android.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+pkgname=oonimkall
+version=$(date -u +%Y%m%dT%H%M%SZ)
+baseurl=https://api.bintray.com/content/ooni/android/$pkgname/$version/org/ooni/$pkgname/$version
+aarfile=./MOBILE/dist/$pkgname.aar
+pomfile=./MOBILE/dist/$pkgname-$version.pom
+pomtemplate=./MOBILE/template.pom
+user=bassosimone
+cat $pomtemplate|sed "s/@VERSION@/$version/g" > $pomfile
+if [ -z $BINTRAY_API_KEY ]; then
+    echo "FATAL: missing BINTRAY_API_KEY variable" 1>&2
+    exit 1
+fi
+# We currently publish every commit. To cleanup we can fetch all the versions using the
+# <curl -s $user:$BINTRAY_API_KEY https://api.bintray.com/packages/ooni/android/oonimkall>
+# query, which returns a list of versions. From such list, we can delete the versions we
+# don't need using <DELETE /packages/:subject/:repo/:package/versions/:version>.
+curl -#T $aarfile -u $user:$BINTRAY_API_KEY $baseurl/$pkgname-$version.aar?publish=1 >/dev/null
+curl -#T $pomfile -u $user:$BINTRAY_API_KEY $baseurl/$pkgname-$version.pom?publish=1 >/dev/null
+echo "implementation 'org.ooni:oonimkall:$version'"
