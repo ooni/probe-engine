c208771faef928e275c9f529802a797dc40775d6
Simone Basso <bassosimone@gmail.com>
2020-04-02 19:32:01 +0200

    netx/resolver: MockableResolver => Mockable
    Part of https://github.com/ooni/probe-engine/issues/359

--- netx/resolver/mockable.go
+++ netx/resolver/mockable.go
@@ -7,8 +7,8 @@ import (
 	"github.com/ooni/probe-engine/atomicx"
 )
 
-// MockableResolver is a broken resolver.
-type MockableResolver struct {
+// Mockable is a broken resolver.
+type Mockable struct {
 	NumFailures *atomicx.Int64
 	err         error
 	result      []string
@@ -16,14 +16,14 @@ type MockableResolver struct {
 
 // NewMockableResolverThatFails creates a new MockableResolver instance
 // that always returns an error indicating NXDOMAIN.
-func NewMockableResolverThatFails() MockableResolver {
-	return MockableResolver{NumFailures: atomicx.NewInt64(), err: errNotFound}
+func NewMockableResolverThatFails() Mockable {
+	return Mockable{NumFailures: atomicx.NewInt64(), err: errNotFound}
 }
 
 // NewMockableResolverWithResult creates a new MockableResolver
 // instance that always returns the specified result.
-func NewMockableResolverWithResult(r []string) MockableResolver {
-	return MockableResolver{NumFailures: atomicx.NewInt64(), result: r}
+func NewMockableResolverWithResult(r []string) Mockable {
+	return Mockable{NumFailures: atomicx.NewInt64(), result: r}
 }
 
 var errNotFound = &net.DNSError{
@@ -31,7 +31,7 @@ var errNotFound = &net.DNSError{
 }
 
 // LookupHost returns the IP addresses of a host
-func (c MockableResolver) LookupHost(ctx context.Context, hostname string) ([]string, error) {
+func (c Mockable) LookupHost(ctx context.Context, hostname string) ([]string, error) {
 	if c.err != nil {
 		c.NumFailures.Add(1)
 		return nil, c.err
