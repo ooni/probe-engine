2f73887e1f03eba68ad4c2cc4decb421ff296702
Simone Basso <bassosimone@gmail.com>
2020-04-02 20:29:30 +0200

    netx/resolver/dnsoverhttps: rewrite to only use unit tests
    Part of https://github.com/ooni/probe-engine/issues/359

--- netx/resolver/dnsoverhttps_test.go
+++ netx/resolver/dnsoverhttps_test.go
@@ -1,81 +1,116 @@
-package resolver
+package resolver_test
 
 import (
+	"bytes"
+	"context"
 	"errors"
 	"io/ioutil"
 	"net/http"
 	"strings"
 	"testing"
+
+	"github.com/ooni/probe-engine/netx/resolver"
 )
 
-func TestIntegrationDNSOverHTTPSSuccess(t *testing.T) {
-	const queryURL = "https://cloudflare-dns.com/dns-query"
-	transport := NewDNSOverHTTPS(
-		http.DefaultClient, queryURL,
-	)
-	if transport.Network() != "doh" {
-		t.Fatal("invalid network")
+func TestUnitDNSOverHTTPSNewRequestFailure(t *testing.T) {
+	const invalidURL = "\t"
+	txp := resolver.NewDNSOverHTTPS(http.DefaultClient, invalidURL)
+	data, err := txp.RoundTrip(context.Background(), nil)
+	if err == nil {
+		t.Fatal("expected an error here")
 	}
-	if transport.Address() != queryURL {
-		t.Fatal("invalid address")
+	if data != nil {
+		t.Fatal("expected no response here")
 	}
-	err := threeRounds(transport)
+}
+
+func TestUnitDNSOverHTTPSClientDoFailure(t *testing.T) {
+	txp := resolver.DNSOverHTTPS{
+		Do: func(*http.Request) (*http.Response, error) {
+			return nil, errors.New("mocked error")
+		},
+		URL: "https://cloudflare-dns.com/dns-query",
+	}
+	data, err := txp.RoundTrip(context.Background(), nil)
+	if err == nil {
+		t.Fatal("expected an error here")
+	}
+	if data != nil {
+		t.Fatal("expected no response here")
+	}
+}
+
+func TestUnitDNSOverHTTPSHTTPFailure(t *testing.T) {
+	txp := resolver.DNSOverHTTPS{
+		Do: func(*http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 500,
+				Body:       ioutil.NopCloser(strings.NewReader("")),
+			}, nil
+		},
+		URL: "https://cloudflare-dns.com/dns-query",
+	}
+	data, err := txp.RoundTrip(context.Background(), nil)
+	if err == nil {
+		t.Fatal("expected an error here")
+	}
+	if data != nil {
+		t.Fatal("expected no response here")
+	}
+}
+
+func TestUnitDNSOverHTTPSMissingContentType(t *testing.T) {
+	txp := resolver.DNSOverHTTPS{
+		Do: func(*http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 200,
+				Body:       ioutil.NopCloser(strings.NewReader("")),
+			}, nil
+		},
+		URL: "https://cloudflare-dns.com/dns-query",
+	}
+	data, err := txp.RoundTrip(context.Background(), nil)
+	if err == nil {
+		t.Fatal("expected an error here")
+	}
+	if data != nil {
+		t.Fatal("expected no response here")
+	}
+}
+
+func TestUnitDNSOverHTTPSSuccess(t *testing.T) {
+	body := []byte("AAA")
+	txp := resolver.DNSOverHTTPS{
+		Do: func(*http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 200,
+				Body:       ioutil.NopCloser(bytes.NewReader(body)),
+				Header: http.Header{
+					"Content-Type": []string{"application/dns-message"},
+				},
+			}, nil
+		},
+		URL: "https://cloudflare-dns.com/dns-query",
+	}
+	data, err := txp.RoundTrip(context.Background(), nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-}
-
-func TestIntegrationDNSOverHTTPSNewRequestFailure(t *testing.T) {
-	transport := NewDNSOverHTTPS(
-		http.DefaultClient, "\t", // invalid URL
-	)
-	err := threeRounds(transport)
-	if err == nil {
-		t.Fatal("expected an error here")
+	if !bytes.Equal(data, body) {
+		t.Fatal("not the response we expected")
 	}
 }
 
-func TestIntegrationDNSOverHTTPSClientDoFailure(t *testing.T) {
-	transport := NewDNSOverHTTPS(
-		http.DefaultClient, "https://cloudflare-dns.com/dns-query",
-	)
-	transport.clientDo = func(*http.Request) (*http.Response, error) {
-		return nil, errors.New("mocked error")
+func TestUnitDNSOverHTTPTransportOK(t *testing.T) {
+	const queryURL = "https://cloudflare-dns.com/dns-query"
+	txp := resolver.NewDNSOverHTTPS(http.DefaultClient, queryURL)
+	if txp.Network() != "doh" {
+		t.Fatal("invalid network")
 	}
-	err := threeRounds(transport)
-	if err == nil {
-		t.Fatal("expected an error here")
+	if txp.RequiresPadding() != true {
+		t.Fatal("should require padding")
 	}
-}
-
-func TestIntegrationDNSOverHTTPSHTTPFailure(t *testing.T) {
-	transport := NewDNSOverHTTPS(
-		http.DefaultClient, "https://cloudflare-dns.com/dns-query",
-	)
-	transport.clientDo = func(*http.Request) (*http.Response, error) {
-		return &http.Response{
-			StatusCode: 500,
-			Body:       ioutil.NopCloser(strings.NewReader("")),
-		}, nil
-	}
-	err := threeRounds(transport)
-	if err == nil {
-		t.Fatal("expected an error here")
-	}
-}
-
-func TestIntegrationDNSOverHTTPSMissingHeader(t *testing.T) {
-	transport := NewDNSOverHTTPS(
-		http.DefaultClient, "https://cloudflare-dns.com/dns-query",
-	)
-	transport.clientDo = func(*http.Request) (*http.Response, error) {
-		return &http.Response{
-			StatusCode: 200,
-			Body:       ioutil.NopCloser(strings.NewReader("")),
-		}, nil
-	}
-	err := threeRounds(transport)
-	if err == nil {
-		t.Fatal("expected an error here")
+	if txp.Address() != queryURL {
+		t.Fatal("invalid address")
 	}
 }
