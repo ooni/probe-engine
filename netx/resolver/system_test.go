dcd735ca29fb488b9419e4ac545aa32d76a5b8a7
Simone Basso <bassosimone@gmail.com>
2020-04-03 15:10:10 +0200

    netx/resolver: standalone emitter resolver w/ unit tests (#465)
    Part of https://github.com/ooni/probe-engine/issues/359

--- netx/resolver/system_test.go
+++ netx/resolver/system_test.go
@@ -7,27 +7,6 @@ import (
 	"github.com/ooni/probe-engine/netx/resolver"
 )
 
-func TestUnitSystemResolverTransport(t *testing.T) {
-	r := resolver.SystemResolver{}
-	transport := r.Transport()
-	reply, err := transport.RoundTrip(context.Background(), nil)
-	if err == nil || err.Error() != "not implemented" {
-		t.Fatal("not the error we expected")
-	}
-	if reply != nil {
-		t.Fatal("expected nil reply here")
-	}
-	if transport.Address() != "" {
-		t.Fatal("invalid address")
-	}
-	if transport.Network() != "system" {
-		t.Fatal("invalid network")
-	}
-	if transport.RequiresPadding() != false {
-		t.Fatal("we should require padding here")
-	}
-}
-
 func TestIntegrationSystemResolverLookupHost(t *testing.T) {
 	r := resolver.SystemResolver{}
 	addrs, err := r.LookupHost(context.Background(), "dns.google.com")
