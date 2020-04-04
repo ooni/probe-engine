1036393958e64ddef20ce37e35d9d77ef223b07b
Simone Basso <bassosimone@gmail.com>
2020-04-04 18:53:10 +0200

    netx: make sure all pkgs have 100% coverage
    Part of https://github.com/ooni/probe-engine/issues/359

--- netx/handlers/handlers_test.go
+++ netx/handlers/handlers_test.go
@@ -10,4 +10,10 @@ import (
 func TestIntegration(t *testing.T) {
 	handlers.NoHandler.OnMeasurement(modelx.Measurement{})
 	handlers.StdoutHandler.OnMeasurement(modelx.Measurement{})
+	saver := handlers.SavingHandler{}
+	saver.OnMeasurement(modelx.Measurement{})
+	events := saver.Read()
+	if len(events) != 1 {
+		t.Fatal("invalid number of events")
+	}
 }
