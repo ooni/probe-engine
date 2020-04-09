1b9fcb689e09f86d94d81aab9072c5ce7d9ce7c9
Simone Basso <bassosimone@gmail.com>
2020-04-09 15:47:46 +0200

    Start building CLI stuff

--- build-cli.bash
+++ build-cli.bash
@@ -2,21 +2,30 @@
 set -ex
 case $1 in
   darwin|linux)
-    dll=libooniffi.so;;
+    dll=libooniffi.so
+    exe=miniooni
+    ;;
   windows)
     export CC=${CC:-x86_64-w64-mingw32-gcc}
-    dll=ooniffi.dll;;
+    dll=ooniffi.dll
+    exe=miniooni.exe
+    ;;
   *)
     echo "usage: $0 darwin|linux|windows" 1>&2
     exit 1
 esac
 export CGO_ENABLED=1 GOOS=$1 GOARCH=amd64
-out=./FFI/$GOOS/$GOARCH
-go build -buildmode c-shared -tags nomk -ldflags='-s -w' \
-    -o $out/$dll ./libooniffi/...
+out=./CLI/$GOOS/$GOARCH
+function build() {
+  go build -tags nomk -ldflags="-s -w" $@
+}
+build -o $out/$exe ./cmd/miniooni
+build -buildmode c-shared -o $out/$dll ./libooniffi/...
 rm -f $out/libooniffi.h
-cp ./libooniffi/ooniffi.h $out/
 case $1 in
+  linux)
+    chmod +x $out/$dll
+    ;;
   windows)
     DLLTOOL=${DLLTOOL:-x86_64-w64-mingw32-dlltool}
     $DLLTOOL -d $out/ooniffi.def -l $out/ooniffi.lib $out/ooniffi.dll
