f0b3aeadfb91489e7d26791948a78d1cd000ab8a
Simone Basso <bassosimone@gmail.com>
2020-04-09 18:46:43 +0200

    Implement drop-in API-and-ABI-compatible MK replacement (#492)
    This PR implements a drop-in API-and-ABI-compatible replacement for MK that can be used by FFI clients. The only FFI client I know is the soon-to-be-deprecated MK binary. I am not going to invest much time in automatically shipping the library we can create from this code. I am happy enough that we run tests and we have nearly full coverage. The only code bit without coverage is ~10 lines of obvious C code that just adds/removes the `const` qualifier.
    
    With this functionality in, I am going to call https://github.com/measurement-kit/measurement-kit/issues/1913 done. Of course, we still need to rewrite everything in Go, but all the use cases covered by MK are nonetheless now fully covered. This is a precondition for https://github.com/measurement-kit/measurement-kit/issues/1921: if people need a C library, we can do the extra mile and build it for them.

---
+++ libooniffi/ooniffi.h
@@ -0,0 +1,33 @@
+#ifndef INCLUDE_OONIFFI_H_
+#define INCLUDE_OONIFFI_H_
+
+#include <stdint.h>
+#include <stdlib.h>
+
+/*
+ * API/ABI compatible with Measurement Kit v0.10.11 [1].
+ *
+ * Just replace `mk_` with `ooniffi_` and recompile.
+ *
+ * .. [1] https://github.com/measurement-kit/measurement-kit/tree/v0.10.11/
+ */
+
+typedef struct ooniffi_task_ ooniffi_task_t;
+typedef struct ooniffi_event_ ooniffi_event_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern ooniffi_task_t *ooniffi_task_start(const char *settings);
+extern ooniffi_event_t *ooniffi_task_wait_for_next_event(ooniffi_task_t *task);
+extern int ooniffi_task_is_done(ooniffi_task_t *task);
+extern void ooniffi_task_interrupt(ooniffi_task_t *task);
+extern const char *ooniffi_event_serialization(ooniffi_event_t *str);
+extern void ooniffi_event_destroy(ooniffi_event_t *str);
+extern void ooniffi_task_destroy(ooniffi_task_t *task);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* INCLUDE_OONIFFI_H_ */
