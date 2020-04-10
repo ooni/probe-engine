233cd6285091414c93fb102714c6b7c211354934
Simone Basso <bassosimone@gmail.com>
2020-04-10 10:43:02 +0200

    Build miniooni at every push (#493)

---
+++ build-cli.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -ex
+case $1 in
+  _linux)
+    apk add gcc musl-dev
+    go build -tags "netgo nomk" -ldflags='-s -w -extldflags "-static"' \
+      -o ./CLI/linux/amd64/miniooni ./cmd/miniooni;;
+  darwin)
+    go build -tags nomk -o ./CLI/darwin/amd64 ./cmd/miniooni;;
+  linux)
+    docker run -v`pwd`:/ooni -w/ooni golang:alpine ./build-cli.sh _linux;;
+  windows)
+    export CC=x86_64-w64-mingw32-gcc CGO_ENABLED=1 GOOS=windows GOARCH=amd64
+    go build -tags nomk -o ./CLI/windows/amd64 ./cmd/miniooni;;
+  *)
+    echo "usage: $0 darwin|linux|windows" 1>&2
+    exit 1
+esac
