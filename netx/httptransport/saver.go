6332abd3eb5ed66904cc58b9d6cdbd1fe4860b33
Simone Basso <bassosimone@gmail.com>
2020-04-29 19:04:59 +0200

    netx/httptransport: reintroduce http/httptrace (#557)
    In https://github.com/ooni/probe-engine/pull/556 I was optimistic. It seems
    it's difficult to generate a HAR w/o using http/httptrace.
    
    Thus, reimplement measuring using http/httptrace. This time, however, do that
    in a separate round tripper, to be more tidy.
    
    Part of https://github.com/ooni/probe-engine/issues/543

--- netx/httptransport/saver.go
+++ netx/httptransport/saver.go
@@ -2,10 +2,39 @@ package httptransport
 
 import (
 	"net/http"
+	"net/http/httptrace"
 	"time"
 
 	"github.com/ooni/probe-engine/netx/trace"
 )
+
+// SaverPerformanceHTTPTransport is a RoundTripper that saves
+// performance events during the round trip
+type SaverPerformanceHTTPTransport struct {
+	RoundTripper
+	Saver *trace.Saver
+}
+
+// RoundTrip implements RoundTripper.RoundTrip
+func (txp SaverPerformanceHTTPTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	tracep := httptrace.ContextClientTrace(req.Context())
+	if tracep == nil {
+		tracep = &httptrace.ClientTrace{
+			WroteHeaders: func() {
+				txp.Saver.Write(trace.Event{Name: "http_wrote_headers", Time: time.Now()})
+			},
+			WroteRequest: func(httptrace.WroteRequestInfo) {
+				txp.Saver.Write(trace.Event{Name: "http_wrote_request", Time: time.Now()})
+			},
+			GotFirstResponseByte: func() {
+				txp.Saver.Write(trace.Event{
+					Name: "http_first_response_byte", Time: time.Now()})
+			},
+		}
+		req = req.WithContext(httptrace.WithClientTrace(req.Context(), tracep))
+	}
+	return txp.RoundTripper.RoundTrip(req)
+}
 
 // SaverHTTPTransport is a RoundTripper that saves events
 type SaverHTTPTransport struct {
@@ -34,4 +63,5 @@ func (txp SaverHTTPTransport) RoundTrip(req *http.Request) (*http.Response, erro
 	return resp, err
 }
 
+var _ RoundTripper = SaverPerformanceHTTPTransport{}
 var _ RoundTripper = SaverHTTPTransport{}
