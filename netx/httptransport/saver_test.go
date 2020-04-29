44aacc2f4458150ad007aa629addc378e8732586
Simone Basso <bassosimone@gmail.com>
2020-04-29 18:44:34 +0200

    Significantly simplify the implementation

--- netx/httptransport/saver_test.go
+++ netx/httptransport/saver_test.go
@@ -1,185 +1,14 @@
 package httptransport_test
 
 import (
-	"bytes"
-	"context"
 	"errors"
-	"io/ioutil"
 	"net/http"
 	"testing"
 	"time"
 
-	"github.com/ooni/probe-engine/netx/bytecounter"
-	"github.com/ooni/probe-engine/netx/dialer"
 	"github.com/ooni/probe-engine/netx/httptransport"
 	"github.com/ooni/probe-engine/netx/trace"
 )
-
-func TestUnitSaverPerformanceHTTPTransportSuccessContext(t *testing.T) {
-	saver := &trace.Saver{}
-	txp := httptransport.SaverPerformanceHTTPTransport{
-		RoundTripper: httptransport.FakeTransport{
-			Resp: &http.Response{
-				Body:       ioutil.NopCloser(bytes.NewReader(nil)),
-				StatusCode: 200,
-			},
-		},
-		Saver: saver,
-	}
-	req, err := http.NewRequest("GET", "https://www.google.com", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	ctx, cancel := context.WithCancel(context.Background())
-	cancel() // immediately fail
-	resp, err := txp.RoundTrip(req.WithContext(ctx))
-	if !errors.Is(err, context.Canceled) {
-		t.Fatal("not the error we expected")
-	}
-	if resp != nil {
-		t.Fatal("expected nil response here")
-	}
-	ev := saver.Read()
-	if len(ev) != 0 {
-		t.Fatal("expected zero events")
-	}
-}
-
-func TestIntegrationSaverPerformanceHTTPTransportSuccess(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping test in short mode")
-	}
-	saver := &trace.Saver{}
-	txp := httptransport.SaverPerformanceHTTPTransport{
-		RoundTripper: http.DefaultTransport.(*http.Transport),
-		Saver:        saver,
-	}
-	req, err := http.NewRequest("GET", "https://www.google.com", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	resp, err := txp.RoundTrip(req)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if resp == nil {
-		t.Fatal("expected non nil response here")
-	}
-	ev := saver.Read()
-	if len(ev) != 3 {
-		t.Fatal("expected two events")
-	}
-	//
-	if ev[0].Name != "http_wrote_headers" {
-		t.Fatal("unexpected Name")
-	}
-	if !ev[0].Time.Before(time.Now()) {
-		t.Fatal("unexpected Time")
-	}
-	//
-	if ev[1].Name != "http_wrote_request" {
-		t.Fatal("unexpected Name")
-	}
-	if !ev[1].Time.After(ev[0].Time) {
-		t.Fatal("unexpected Time")
-	}
-	//
-	if ev[2].Name != "http_first_response_byte" {
-		t.Fatal("unexpected Name")
-	}
-	if !ev[2].Time.After(ev[1].Time) {
-		t.Fatal("unexpected Time")
-	}
-}
-
-func TestIntegrationSaverPerformanceHTTPTransportSuccessByteCounting(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping test in short mode")
-	}
-	expCounter := bytecounter.New()
-	sessCounter := bytecounter.New()
-	ctx := context.Background()
-	ctx = dialer.WithExperimentByteCounter(ctx, expCounter)
-	ctx = dialer.WithSessionByteCounter(ctx, sessCounter)
-	saver := &trace.Saver{}
-	txp := httptransport.SaverPerformanceHTTPTransport{
-		RoundTripper: httptransport.New(httptransport.Config{
-			ContextByteCounting: true,
-		}),
-		Saver: saver,
-	}
-	req, err := http.NewRequest("GET", "https://www.google.com", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	resp, err := txp.RoundTrip(req.WithContext(ctx))
-	if err != nil {
-		t.Fatal(err)
-	}
-	if resp == nil {
-		t.Fatal("expected non nil response here")
-	}
-	if expCounter.Received.Load() <= 0 || expCounter.Sent.Load() <= 0 {
-		t.Fatal("broken experiment counter")
-	}
-	if sessCounter.Received.Load() <= 0 || sessCounter.Received.Load() <= 0 {
-		t.Fatal("broken session counter")
-	}
-}
-
-func TestUnitSaverPerformanceHTTPTransportFailure(t *testing.T) {
-	expected := errors.New("mocked error")
-	saver := &trace.Saver{}
-	txp := httptransport.SaverPerformanceHTTPTransport{
-		RoundTripper: httptransport.FakeTransport{
-			Err: expected,
-		},
-		Saver: saver,
-	}
-	req, err := http.NewRequest("GET", "https://www.google.com", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	resp, err := txp.RoundTrip(req)
-	if !errors.Is(err, expected) {
-		t.Fatal("not the error we expected")
-	}
-	if resp != nil {
-		t.Fatal("expected nil response here")
-	}
-	ev := saver.Read()
-	if len(ev) != 0 {
-		t.Fatal("expected zero events")
-	}
-}
-
-func TestUnitSaverPerformanceHTTPTransportFailureContext(t *testing.T) {
-	expected := errors.New("mocked error")
-	saver := &trace.Saver{}
-	txp := httptransport.SaverPerformanceHTTPTransport{
-		RoundTripper: httptransport.FakeTransport{
-			Err: expected,
-		},
-		Saver: saver,
-	}
-	req, err := http.NewRequest("GET", "https://www.google.com", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	ctx, cancel := context.WithCancel(context.Background())
-	cancel() // cause immediate failure
-	resp, err := txp.RoundTrip(req.WithContext(ctx))
-	if !errors.Is(err, context.Canceled) {
-		t.Fatal("not the error we expected")
-	}
-	if resp != nil {
-		t.Fatal("expected nil response here")
-	}
-	ev := saver.Read()
-	if len(ev) != 0 {
-		t.Fatal("expected zero events")
-	}
-}
 
 func TestUnitSaverHTTPTransportFailure(t *testing.T) {
 	expected := errors.New("mocked error")
@@ -298,5 +127,51 @@ func TestIntegrationSaverHTTPTransportSuccess(t *testing.T) {
 	}
 	if !ev[1].Time.After(ev[0].Time) {
 		t.Fatal("unexpected Time")
+	}
+}
+
+func TestIntegrationSaverPerformanceNoMultipleEvents(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode")
+	}
+	saver := &trace.Saver{}
+	// register twice - do we see events twice?
+	txp := httptransport.SaverPerformanceHTTPTransport{
+		RoundTripper: http.DefaultTransport.(*http.Transport),
+		Saver:        saver,
+	}
+	txp = httptransport.SaverPerformanceHTTPTransport{
+		RoundTripper: txp,
+		Saver:        saver,
+	}
+	req, err := http.NewRequest("GET", "https://www.google.com", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp, err := txp.RoundTrip(req)
+	if err != nil {
+		t.Fatal("not the error we expected")
+	}
+	if resp == nil {
+		t.Fatal("expected non nil response here")
+	}
+	ev := saver.Read()
+	// we should specifically see the events not attached to any
+	// context being submitted twice. This is fine because they are
+	// explicit, while the context is implicit and hence leads to
+	// more subtle bugs. For example, this happens when you measure
+	// every event and combine HTTP with DoH.
+	if len(ev) != 3 {
+		t.Fatal("expected three events")
+	}
+	expected := []string{
+		"http_wrote_headers",       // measured with context
+		"http_wrote_request",       // measured with context
+		"http_first_response_byte", // measured with context
+	}
+	for i := 0; i < len(expected); i++ {
+		if ev[i].Name != expected[i] {
+			t.Fatal("unexpected event name")
+		}
 	}
 }
