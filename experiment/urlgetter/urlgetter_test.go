0dedf5470247544859a45fe60fb016a7736ce0fb
Simone Basso <bassosimone@gmail.com>
2020-05-03 23:16:08 +0200

    experiment/urlgetter: include DNSCache in TestKeys
    This simplifies analysing the measurement JSON.
    
    Part of https://github.com/ooni/probe-engine/issues/543

--- experiment/urlgetter/urlgetter_test.go
+++ experiment/urlgetter/urlgetter_test.go
@@ -35,3 +35,33 @@ func TestMeasurer(t *testing.T) {
 		t.Fatal("not the expected number of extensions")
 	}
 }
+
+func TestMeasurerDNSCache(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+	m := urlgetter.NewExperimentMeasurer(urlgetter.Config{
+		DNSCache: "dns.google 8.8.8.8 8.8.4.4",
+	})
+	if m.ExperimentName() != "urlgetter" {
+		t.Fatal("invalid experiment name")
+	}
+	if m.ExperimentVersion() != "0.0.3" {
+		t.Fatal("invalid experiment version")
+	}
+	measurement := new(model.Measurement)
+	measurement.Input = "https://www.google.com"
+	err := m.Run(
+		ctx, &mockable.ExperimentSession{},
+		measurement, handler.NewPrinterCallbacks(log.Log),
+	)
+	if !errors.Is(err, context.Canceled) {
+		t.Fatal("not the error we expected")
+	}
+	if len(measurement.Extensions) != 4 {
+		t.Fatal("not the expected number of extensions")
+	}
+	tk := measurement.TestKeys.(urlgetter.TestKeys)
+	if len(tk.DNSCache) != 1 || tk.DNSCache[0] != "dns.google 8.8.8.8 8.8.4.4" {
+		t.Fatal("invalid tk.DNSCache")
+	}
+}
