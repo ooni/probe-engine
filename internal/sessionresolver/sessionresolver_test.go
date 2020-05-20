57e026fd4239a5776fddf61064d0f0037035b51c
Simone Basso <bassosimone@gmail.com>
2020-05-20 02:54:13 +0200

    session: use cloudflare doh by default, system as fallback
    This is useful at least as far as we're talking with OONI servers.
    
    Closes https://github.com/ooni/probe-engine/issues/521.
    
    Part of https://github.com/ooni/probe/issues/887.

---
+++ internal/sessionresolver/sessionresolver_test.go
@@ -0,0 +1,34 @@
+package sessionresolver_test
+
+import (
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/ooni/probe-engine/internal/sessionresolver"
+	"github.com/ooni/probe-engine/netx/httptransport"
+)
+
+func TestIntegration(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode")
+	}
+	reso := sessionresolver.New(httptransport.Config{})
+	defer reso.CloseIdleConnections()
+	if reso.Network() != "sessionresolver" {
+		t.Fatal("unexpected Network")
+	}
+	if reso.Address() != "" {
+		t.Fatal("unexpected Address")
+	}
+	addrs, err := reso.LookupHost(context.Background(), "antani.ooni.io")
+	if err == nil || !strings.HasSuffix(err.Error(), "no such host") {
+		t.Fatal("not the error we expected")
+	}
+	if addrs != nil {
+		t.Fatal("expected nil addrs here")
+	}
+	if reso.PrimaryFailure.Load() != 1 || reso.FallbackFailure.Load() != 1 {
+		t.Fatal("not the counters we expected to see here")
+	}
+}
