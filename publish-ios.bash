d08940c3e98aceb349e0c2e3cd14e6e61fd4f07e
Simone Basso <bassosimone@gmail.com>
2020-05-27 17:13:53 +0200

    this should work

--- publish-ios.bash
+++ publish-ios.bash
@@ -2,22 +2,22 @@
 set -e
 pkgname=oonimkall
 version=$(date -u +%Y.%m.%d-%H%M%S)
-baseurl=https://api.bintray.com/content/ooni/ios/$pkgname/$pkgversion
+baseurl=https://api.bintray.com/content/ooni/ios/$pkgname/$version
 framework=./MOBILE/dist/$pkgname.framework
 frameworkzip=./MOBILE/dist/$pkgname.framework.zip
 podspecfile=./MOBILE/dist/$pkgname.podspec
 podspectemplate=./MOBILE/template.podspec
 user=bassosimone
+(cd ./MOBILE/dist && rm -f $pkgname.framework.zip && zip $pkgname.framework.zip $pkgname.framework)
 cat $podspectemplate|sed "s/@VERSION@/$version/g" > $podspecfile
 if [ -z $BINTRAY_API_KEY ]; then
     echo "FATAL: missing BINTRAY_API_KEY variable" 1>&2
     exit 1
 fi
-(cd ./MOBILE/dist && zip $pkgname.framework.zip $pkgname.framework)
 # We currently publish every commit. To cleanup we can fetch all the versions using the
 # <curl -s $user:$BINTRAY_API_KEY https://api.bintray.com/packages/ooni/android/oonimkall>
 # query, which returns a list of versions. From such list, we can delete the versions we
 # don't need using <DELETE /packages/:subject/:repo/:package/versions/:version>.
 curl -sT $frameworkzip -u $user:$BINTRAY_API_KEY $baseurl/$pkgname-$version.framework.zip?publish=1 >/dev/null
 curl -sT $podspecfile -u $user:$BINTRAY_API_KEY $baseurl/$pkgname-$version.podspec?publish=1 >/dev/null
-echo "pod 'OONIMKAll', :podspec => 'https://dl.bintray.com/ooni/ios/$pkgname-$pkgversion.podspec'"
+echo "pod 'oonimkall', :podspec => 'https://dl.bintray.com/ooni/ios/$pkgname-$version.podspec'"
