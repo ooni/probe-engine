bece14bc1cdc15a0fac5e1518e9c1aad94ec0d64
Simone Basso <bassosimone@gmail.com>
2020-05-29 15:26:10 +0200

    internal/httpheader: update user-agent we use

--- internal/httpheader/useragent.go
+++ internal/httpheader/useragent.go
@@ -6,7 +6,7 @@ func RandomUserAgent() string {
 	// TODO(bassosimone): this user-agent solution is temporary and we
 	// should instead select one among many user agents. See #147.
 	//
-	// 11.8% as of August 24, 2019 according to https://techblog.willshouse.com/2012/01/03/most-common-user-agents/
-	const ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+	// 12.9% as of May 29, 2020 according to https://techblog.willshouse.com/2012/01/03/most-common-user-agents/
+	const ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"
 	return ua
 }
