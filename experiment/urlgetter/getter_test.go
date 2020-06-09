b352177b15e960b496d4da7ea38c63a5be4125f8
Simone Basso <bassosimone@gmail.com>
2020-06-09 11:29:59 +0200

    urlgetter: allow to configure the method
    We need this to reimplement the telegram experiment in terms of
    the urlgetter experiment, to simplify the code.
    
    See https://github.com/ooni/probe-engine/issues/646.

--- experiment/urlgetter/getter_test.go
+++ experiment/urlgetter/getter_test.go
@@ -71,6 +71,65 @@ func TestGetterWithCancelledContextVanilla(t *testing.T) {
 	}
 }
 
+func TestGetterWithCancelledContextAndMethod(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+	g := urlgetter.Getter{
+		Config:  urlgetter.Config{Method: "POST"},
+		Session: &mockable.ExperimentSession{},
+		Target:  "https://www.google.com",
+	}
+	tk, err := g.Get(ctx)
+	if !errors.Is(err, context.Canceled) {
+		t.Fatal("not the error we expected")
+	}
+	if tk.Agent != "redirect" {
+		t.Fatal("not the Agent we expected")
+	}
+	if tk.BootstrapTime != 0 {
+		t.Fatal("not the BootstrapTime we expected")
+	}
+	if tk.Failure == nil || !strings.HasSuffix(*tk.Failure, "context canceled") {
+		t.Fatal("not the Failure we expected")
+	}
+	if len(tk.NetworkEvents) != 3 {
+		t.Fatal("not the NetworkEvents we expected")
+	}
+	if tk.NetworkEvents[0].Operation != "http_transaction_start" {
+		t.Fatal("not the NetworkEvents[0].Operation we expected")
+	}
+	if tk.NetworkEvents[1].Operation != "http_request_metadata" {
+		t.Fatal("not the NetworkEvents[1].Operation we expected")
+	}
+	if tk.NetworkEvents[2].Operation != "http_transaction_done" {
+		t.Fatal("not the NetworkEvents[2].Operation we expected")
+	}
+	if len(tk.Queries) != 0 {
+		t.Fatal("not the Queries we expected")
+	}
+	if len(tk.TCPConnect) != 0 {
+		t.Fatal("not the TCPConnect we expected")
+	}
+	if len(tk.Requests) != 1 {
+		t.Fatal("not the Requests we expected")
+	}
+	if tk.Requests[0].Request.Method != "POST" {
+		t.Fatal("not the Method we expected")
+	}
+	if tk.Requests[0].Request.URL != "https://www.google.com" {
+		t.Fatal("not the URL we expected")
+	}
+	if tk.SOCKSProxy != "" {
+		t.Fatal("not the SOCKSProxy we expected")
+	}
+	if len(tk.TLSHandshakes) != 0 {
+		t.Fatal("not the TLSHandshakes we expected")
+	}
+	if tk.Tunnel != "" {
+		t.Fatal("not the Tunnel we expected")
+	}
+}
+
 func TestGetterWithCancelledContextNoFollowRedirects(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	cancel()
