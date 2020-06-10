b18703cdb8985333c01d7d5e4040191ce4cd1b5c
Simone Basso <bassosimone@gmail.com>
2020-06-10 13:38:09 +0200

    Update to ooni/probe-assets@20200610133507

--- internal/resources/assets.go
+++ internal/resources/assets.go
@@ -2,7 +2,7 @@ package resources
 
 const (
 	// Version contains the assets version.
-	Version = 20200529153246
+	Version = 20200610133507
 
 	// ASNDatabaseName is the ASN-DB file name
 	ASNDatabaseName = "asn.mmdb"
@@ -33,18 +33,18 @@ type ResourceInfo struct {
 // All contains info on all known assets.
 var All = map[string]ResourceInfo{
 	"asn.mmdb": {
-		URLPath:  "/releases/download/20200529153246/asn.mmdb.gz",
+		URLPath:  "/releases/download/20200610133507/asn.mmdb.gz",
 		GzSHA256: "19c7184dcd6339d33aea22811ed015eeb4d8ce19606bba2cb5bdc1b205b602f0",
 		SHA256:   "a89cef5b583607891e2e3fec620efcd3276c51684909351433adc6c870854089",
 	},
 	"ca-bundle.pem": {
-		URLPath:  "/releases/download/20200529153246/ca-bundle.pem.gz",
+		URLPath:  "/releases/download/20200610133507/ca-bundle.pem.gz",
 		GzSHA256: "08070cbe24c8895d18bb20ccd746ff7409f1947094a1a47aa59993f588474485",
 		SHA256:   "adf770dfd574a0d6026bfaa270cb6879b063957177a991d453ff1d302c02081f",
 	},
 	"country.mmdb": {
-		URLPath:  "/releases/download/20200529153246/country.mmdb.gz",
-		GzSHA256: "5d7b938a66e2018d9cec356a48c1042d85f1a47dbcf2f56247659d5cc6534e4c",
-		SHA256:   "c38d94572026fadf86f5ee8f2258c3712d519c98b9f87c0119f6d65cd120d5ad",
+		URLPath:  "/releases/download/20200610133507/country.mmdb.gz",
+		GzSHA256: "a3b13c78c149da4fa8bd63bbb0c25ead9737c1da3bf2e3199f2bca86d73c01b9",
+		SHA256:   "83f369ddcb560862996848b600ce1e5353659dcb9424c5e9dd7f6d980fc56a60",
 	},
 }
