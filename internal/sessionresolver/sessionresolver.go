15b0e0705a7c06a1feb6266d765097eec3c6460d
Simone Basso <bassosimone@gmail.com>
2020-07-12 19:26:28 +0200

    Update sessionresolver.go

--- internal/sessionresolver/sessionresolver.go
+++ internal/sessionresolver/sessionresolver.go
@@ -42,11 +42,10 @@ func (r *Resolver) CloseIdleConnections() {
 
 // LookupHost implements Resolver.LookupHost
 func (r *Resolver) LookupHost(ctx context.Context, hostname string) ([]string, error) {
-	// We doing this like Firefox trr 2 mode. See here:
+	// Algorithm similar to Firefox TRR2 mode. See:
 	// https://wiki.mozilla.org/Trusted_Recursive_Resolver#DNS-over-HTTPS_Prefs_in_Firefox
-	// And here: network.trr.request_timeout_ms
-	// But we use a higher timeout than Firefox to be safe and use DoH more
-	// often if possible
+	// We use a higher timeout than Firefox's timeout (1.5s) to be on the safe side
+	// and therefore see to use DoH more often.
 	trr2, cancel := context.WithTimeout(ctx, 4*time.Second)
 	defer cancel()
 	addrs, err := r.Primary.LookupHost(trr2, hostname)
