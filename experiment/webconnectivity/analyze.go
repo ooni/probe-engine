7628edc6991bd8c132fb0b47926656706b61f53a
Simone Basso <bassosimone@gmail.com>
2020-07-14 20:40:46 +0200

    webconnectivity: refactor DNS analysis in a separate file
    Part of https://github.com/ooni/probe-engine/issues/776

--- experiment/webconnectivity/analyze.go
+++ experiment/webconnectivity/analyze.go
@@ -1,18 +1,13 @@
 package webconnectivity
 
 import (
-	"net"
-	"net/url"
 	"regexp"
 	"strings"
-
-	"github.com/ooni/probe-engine/netx/modelx"
 )
 
 // AnalysisResult contains the results of the analysis performed on the
 // client. We obtain it by comparing the measurement and the control.
 type AnalysisResult struct {
-	DNSConsistency  string   `json:"dns_consistency"`
 	BodyLengthMatch *bool    `json:"body_length_match"`
 	BodyProportion  *float64 `json:"body_proportion"`
 	StatusCodeMatch *bool    `json:"status_code_match"`
@@ -26,106 +21,10 @@ type AnalysisResult struct {
 // comparing the measurement (tk.TestKeys) and the control (tk.Control). This
 // function will return the results of the analysis.
 func Analyze(target string, tk *TestKeys) (out AnalysisResult) {
-	URL, err := url.Parse(target)
-	if err != nil {
-		return // should not happen in practice
-	}
-	out.DNSConsistency = DNSConsistency(URL, tk)
 	out.BodyLengthMatch, out.BodyProportion = BodyLengthChecks(tk)
 	out.StatusCodeMatch = StatusCodeMatch(tk)
 	out.HeadersMatch = HeadersMatch(tk)
 	out.TitleMatch = TitleMatch(tk)
-	return
-}
-
-// ControlDNSNameError is the error returned by the control on NXDOMAIN
-const ControlDNSNameError = "dns_name_error"
-
-var (
-	// AnalyzeDNSConsistent indicates that the measurement and the
-	// control have consistent DNS results.
-	AnalyzeDNSConsistent = "consistent"
-
-	// AnalyzeDNSInconsistent indicates that the measurement and the
-	// control have inconsistent DNS results.
-	AnalyzeDNSInconsistent = "inconsistent"
-)
-
-// DNSConsistency returns the value of the AnalysisResult.DNSConsistency
-// field that you should set into an AnalysisResult struct.
-//
-// This implementation is a simplified version of the implementation of
-// the same check in Measurement Kit v0.10.11.
-func DNSConsistency(URL *url.URL, tk *TestKeys) (out string) {
-	// 0. start assuming it's not consistent
-	out = AnalyzeDNSInconsistent
-	// 1. flip to consistent if we're targeting an IP address because the
-	// control will actually return dns_name_error in this case.
-	if net.ParseIP(URL.Hostname()) != nil {
-		out = AnalyzeDNSConsistent
-		return
-	}
-	// 2. flip to consistent if the failures are compatible
-	if tk.DNSExperimentFailure != nil && tk.Control.DNS.Failure != nil {
-		switch *tk.Control.DNS.Failure {
-		case ControlDNSNameError: // the control returns this on NXDOMAIN error
-			switch *tk.DNSExperimentFailure {
-			case modelx.FailureDNSNXDOMAINError:
-				out = AnalyzeDNSConsistent
-			}
-		}
-		return
-	}
-	// 3. flip to consistent if measurement and control returned IP addresses
-	// that belong to the same Autonomous System(s).
-	//
-	// This specific check is present in MK's implementation.
-	//
-	// Note that this covers also the cases where the measurement contains only
-	// bogons while the control does not contain bogons.
-	//
-	// Note that this also covers the cases where results are equal.
-	const (
-		inMeasurement = 1 << 0
-		inControl     = 1 << 1
-		inBoth        = inMeasurement | inControl
-	)
-	asnmap := make(map[int64]int)
-	for _, entry := range tk.Queries {
-		for _, answer := range entry.Answers {
-			asnmap[answer.ASN] |= inMeasurement
-		}
-	}
-	for _, asn := range tk.Control.DNS.ASNs {
-		asnmap[asn] |= inControl
-	}
-	for key, value := range asnmap {
-		// zero means that ASN lookup failed
-		if key != 0 && (value&inBoth) == inBoth {
-			out = AnalyzeDNSConsistent
-			return
-		}
-	}
-	// 4. when ASN lookup failed (unlikely), check whether
-	// there is overlap in the returned IP addresses
-	ipmap := make(map[string]int)
-	for _, entry := range tk.Queries {
-		for _, answer := range entry.Answers {
-			// we exclude the case of empty string below
-			ipmap[answer.IPv4] |= inMeasurement
-			ipmap[answer.IPv6] |= inMeasurement
-		}
-	}
-	for _, ip := range tk.Control.DNS.Addrs {
-		ipmap[ip] |= inControl
-	}
-	for key, value := range ipmap {
-		if key != "" && (value&inBoth) == inBoth {
-			out = AnalyzeDNSConsistent
-			return
-		}
-	}
-	// 5. conclude that measurement and control are inconsistent
 	return
 }
 
--- experiment/webconnectivity/analyze_test.go
+++ experiment/webconnectivity/analyze_test.go
@@ -1,8 +1,6 @@
 package webconnectivity_test
 
 import (
-	"io"
-	"net/url"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -10,14 +8,10 @@ import (
 	"github.com/ooni/probe-engine/experiment/webconnectivity"
 	"github.com/ooni/probe-engine/internal/randx"
 	"github.com/ooni/probe-engine/netx/archival"
-	"github.com/ooni/probe-engine/netx/modelx"
 )
 
 func TestAnalyzeInvalidURL(t *testing.T) {
 	out := webconnectivity.Analyze("\t\t\t", nil)
-	if out.DNSConsistency != "" {
-		t.Fatal("unexpected DNSConsistency")
-	}
 	if out.BodyLengthMatch != nil {
 		t.Fatal("unexpected BodyLengthMatch")
 	}
@@ -35,202 +29,6 @@ func TestAnalyzeInvalidURL(t *testing.T) {
 	}
 	if out.Blocking != nil {
 		t.Fatal("unexpected Blocking")
-	}
-}
-
-func TestDNSConsistency(t *testing.T) {
-	measurementFailure := modelx.FailureDNSNXDOMAINError
-	controlFailure := webconnectivity.ControlDNSNameError
-	eofFailure := io.EOF.Error()
-	type args struct {
-		URL *url.URL
-		tk  *webconnectivity.TestKeys
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantOut string
-	}{{
-		name: "when the URL contains an IP address",
-		args: args{
-			URL: &url.URL{
-				Host: "10.0.0.1",
-			},
-			tk: &webconnectivity.TestKeys{
-				Control: webconnectivity.ControlResponse{
-					DNS: webconnectivity.ControlDNSResult{
-						Failure: &controlFailure,
-					},
-				},
-			},
-		},
-		wantOut: webconnectivity.AnalyzeDNSConsistent,
-	}, {
-		name: "when the failures are not compatible",
-		args: args{
-			URL: &url.URL{
-				Host: "www.kerneltrap.org",
-			},
-			tk: &webconnectivity.TestKeys{
-				DNSExperimentFailure: &eofFailure,
-				Control: webconnectivity.ControlResponse{
-					DNS: webconnectivity.ControlDNSResult{
-						Failure: &controlFailure,
-					},
-				},
-			},
-		},
-		wantOut: webconnectivity.AnalyzeDNSInconsistent,
-	}, {
-		name: "when the failures are compatible",
-		args: args{
-			URL: &url.URL{
-				Host: "www.kerneltrap.org",
-			},
-			tk: &webconnectivity.TestKeys{
-				DNSExperimentFailure: &measurementFailure,
-				Control: webconnectivity.ControlResponse{
-					DNS: webconnectivity.ControlDNSResult{
-						Failure: &controlFailure,
-					},
-				},
-			},
-		},
-		wantOut: webconnectivity.AnalyzeDNSConsistent,
-	}, {
-		name: "when the ASNs are equal",
-		args: args{
-			URL: &url.URL{
-				Host: "fancy.dns",
-			},
-			tk: &webconnectivity.TestKeys{
-				TestKeys: urlgetter.TestKeys{
-					Queries: []archival.DNSQueryEntry{{
-						Answers: []archival.DNSAnswerEntry{{
-							ASN: 15169,
-						}, {
-							ASN: 13335,
-						}},
-					}},
-				},
-				Control: webconnectivity.ControlResponse{
-					DNS: webconnectivity.ControlDNSResult{
-						ASNs: []int64{13335, 15169},
-					},
-				},
-			},
-		},
-		wantOut: webconnectivity.AnalyzeDNSConsistent,
-	}, {
-		name: "when the ASNs overlap",
-		args: args{
-			URL: &url.URL{
-				Host: "fancy.dns",
-			},
-			tk: &webconnectivity.TestKeys{
-				TestKeys: urlgetter.TestKeys{
-					Queries: []archival.DNSQueryEntry{{
-						Answers: []archival.DNSAnswerEntry{{
-							ASN: 15169,
-						}, {
-							ASN: 13335,
-						}},
-					}},
-				},
-				Control: webconnectivity.ControlResponse{
-					DNS: webconnectivity.ControlDNSResult{
-						ASNs: []int64{13335, 13335},
-					},
-				},
-			},
-		},
-		wantOut: webconnectivity.AnalyzeDNSConsistent,
-	}, {
-		name: "when the ASNs do not overlap",
-		args: args{
-			URL: &url.URL{
-				Host: "fancy.dns",
-			},
-			tk: &webconnectivity.TestKeys{
-				TestKeys: urlgetter.TestKeys{
-					Queries: []archival.DNSQueryEntry{{
-						Answers: []archival.DNSAnswerEntry{{
-							ASN: 15169,
-						}, {
-							ASN: 15169,
-						}},
-					}},
-				},
-				Control: webconnectivity.ControlResponse{
-					DNS: webconnectivity.ControlDNSResult{
-						ASNs: []int64{13335, 13335},
-					},
-				},
-			},
-		},
-		wantOut: webconnectivity.AnalyzeDNSInconsistent,
-	}, {
-		name: "when ASNs lookup fails but IPs overlap",
-		args: args{
-			URL: &url.URL{
-				Host: "fancy.dns",
-			},
-			tk: &webconnectivity.TestKeys{
-				TestKeys: urlgetter.TestKeys{
-					Queries: []archival.DNSQueryEntry{{
-						Answers: []archival.DNSAnswerEntry{{
-							IPv4: "8.8.8.8",
-							ASN:  0,
-						}, {
-							IPv6: "2001:4860:4860::8844",
-							ASN:  0,
-						}},
-					}},
-				},
-				Control: webconnectivity.ControlResponse{
-					DNS: webconnectivity.ControlDNSResult{
-						Addrs: []string{"8.8.4.4", "2001:4860:4860::8844"},
-						ASNs:  []int64{0, 0},
-					},
-				},
-			},
-		},
-		wantOut: webconnectivity.AnalyzeDNSConsistent,
-	}, {
-		name: "when ASNs lookup fails and IPs do not overlap",
-		args: args{
-			URL: &url.URL{
-				Host: "fancy.dns",
-			},
-			tk: &webconnectivity.TestKeys{
-				TestKeys: urlgetter.TestKeys{
-					Queries: []archival.DNSQueryEntry{{
-						Answers: []archival.DNSAnswerEntry{{
-							IPv4: "8.8.8.8",
-							ASN:  0,
-						}, {
-							IPv6: "2001:4860:4860::8844",
-							ASN:  0,
-						}},
-					}},
-				},
-				Control: webconnectivity.ControlResponse{
-					DNS: webconnectivity.ControlDNSResult{
-						Addrs: []string{"8.8.4.4", "2001:4860:4860::8888"},
-						ASNs:  []int64{0, 0},
-					},
-				},
-			},
-		},
-		wantOut: webconnectivity.AnalyzeDNSInconsistent,
-	}}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			gotOut := webconnectivity.DNSConsistency(tt.args.URL, tt.args.tk)
-			if diff := cmp.Diff(tt.wantOut, gotOut); diff != "" {
-				t.Fatal(diff)
-			}
-		})
 	}
 }
 
