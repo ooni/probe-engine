a3ed06a21c1e7c5995c1cea41bf4ce6107a83507
Simone Basso <bassosimone@gmail.com>
2020-07-23 13:43:11 +0200

    Reinstate iptables tests and run iptables using sudo

--- cmd/jafar/iptables/iptables_linux.go
+++ cmd/jafar/iptables/iptables_linux.go
@@ -3,8 +3,8 @@
 package iptables
 
 import (
-	"github.com/ooni/probe-engine/internal/runtimex"
 	"github.com/ooni/probe-engine/cmd/jafar/shellx"
+	"github.com/ooni/probe-engine/internal/runtimex"
 )
 
 type linuxShell struct{}
@@ -15,56 +15,56 @@ func (s *linuxShell) createChains() (err error) {
 			// JUST KNOW WE'VE BEEN HERE
 		}
 	}()
-	err = shellx.Run("iptables", "-N", "JAFAR_INPUT")
+	err = shellx.Run("sudo", "iptables", "-N", "JAFAR_INPUT")
 	runtimex.PanicOnError(err, "cannot create JAFAR_INPUT chain")
-	err = shellx.Run("iptables", "-N", "JAFAR_OUTPUT")
+	err = shellx.Run("sudo", "iptables", "-N", "JAFAR_OUTPUT")
 	runtimex.PanicOnError(err, "cannot create JAFAR_OUTPUT chain")
-	err = shellx.Run("iptables", "-t", "nat", "-N", "JAFAR_NAT_OUTPUT")
+	err = shellx.Run("sudo", "iptables", "-t", "nat", "-N", "JAFAR_NAT_OUTPUT")
 	runtimex.PanicOnError(err, "cannot create JAFAR_NAT_OUTPUT chain")
-	err = shellx.Run("iptables", "-I", "OUTPUT", "-j", "JAFAR_OUTPUT")
+	err = shellx.Run("sudo", "iptables", "-I", "OUTPUT", "-j", "JAFAR_OUTPUT")
 	runtimex.PanicOnError(err, "cannot insert jump to JAFAR_OUTPUT")
-	err = shellx.Run("iptables", "-I", "INPUT", "-j", "JAFAR_INPUT")
+	err = shellx.Run("sudo", "iptables", "-I", "INPUT", "-j", "JAFAR_INPUT")
 	runtimex.PanicOnError(err, "cannot insert jump to JAFAR_INPUT")
-	err = shellx.Run("iptables", "-t", "nat", "-I", "OUTPUT", "-j", "JAFAR_NAT_OUTPUT")
+	err = shellx.Run("sudo", "iptables", "-t", "nat", "-I", "OUTPUT", "-j", "JAFAR_NAT_OUTPUT")
 	runtimex.PanicOnError(err, "cannot insert jump to JAFAR_NAT_OUTPUT")
 	return nil
 }
 
 func (s *linuxShell) dropIfDestinationEquals(ip string) error {
-	return shellx.Run("iptables", "-A", "JAFAR_OUTPUT", "-d", ip, "-j", "DROP")
+	return shellx.Run("sudo", "iptables", "-A", "JAFAR_OUTPUT", "-d", ip, "-j", "DROP")
 }
 
 func (s *linuxShell) rstIfDestinationEqualsAndIsTCP(ip string) error {
 	return shellx.Run(
-		"iptables", "-A", "JAFAR_OUTPUT", "--proto", "tcp", "-d", ip,
+		"sudo", "iptables", "-A", "JAFAR_OUTPUT", "--proto", "tcp", "-d", ip,
 		"-j", "REJECT", "--reject-with", "tcp-reset",
 	)
 }
 
 func (s *linuxShell) dropIfContainsKeywordHex(keyword string) error {
 	return shellx.Run(
-		"iptables", "-A", "JAFAR_OUTPUT", "-m", "string", "--algo", "kmp",
+		"sudo", "iptables", "-A", "JAFAR_OUTPUT", "-m", "string", "--algo", "kmp",
 		"--hex-string", keyword, "-j", "DROP",
 	)
 }
 
 func (s *linuxShell) dropIfContainsKeyword(keyword string) error {
 	return shellx.Run(
-		"iptables", "-A", "JAFAR_OUTPUT", "-m", "string", "--algo", "kmp",
+		"sudo", "iptables", "-A", "JAFAR_OUTPUT", "-m", "string", "--algo", "kmp",
 		"--string", keyword, "-j", "DROP",
 	)
 }
 
 func (s *linuxShell) rstIfContainsKeywordHexAndIsTCP(keyword string) error {
 	return shellx.Run(
-		"iptables", "-A", "JAFAR_OUTPUT", "-m", "string", "--proto", "tcp", "--algo",
+		"sudo", "iptables", "-A", "JAFAR_OUTPUT", "-m", "string", "--proto", "tcp", "--algo",
 		"kmp", "--hex-string", keyword, "-j", "REJECT", "--reject-with", "tcp-reset",
 	)
 }
 
 func (s *linuxShell) rstIfContainsKeywordAndIsTCP(keyword string) error {
 	return shellx.Run(
-		"iptables", "-A", "JAFAR_OUTPUT", "-m", "string", "--proto", "tcp", "--algo",
+		"sudo", "iptables", "-A", "JAFAR_OUTPUT", "-m", "string", "--proto", "tcp", "--algo",
 		"kmp", "--string", keyword, "-j", "REJECT", "--reject-with", "tcp-reset",
 	)
 }
@@ -74,7 +74,7 @@ func (s *linuxShell) hijackDNS(address string) error {
 	// secure network feature. Our transparent proxies will use DoT, in order
 	// to bypass this restriction and avoid routing loop.
 	return shellx.Run(
-		"iptables", "-t", "nat", "-A", "JAFAR_NAT_OUTPUT", "-p", "udp",
+		"sudo", "iptables", "-t", "nat", "-A", "JAFAR_NAT_OUTPUT", "-p", "udp",
 		"--dport", "53", "-j", "DNAT", "--to", address,
 	)
 }
@@ -83,7 +83,7 @@ func (s *linuxShell) hijackHTTPS(address string) error {
 	// We need to whitelist root otherwise the traffic sent by Jafar
 	// itself will match the rule and loop.
 	return shellx.Run(
-		"iptables", "-t", "nat", "-A", "JAFAR_NAT_OUTPUT", "-p", "tcp",
+		"sudo", "iptables", "-t", "nat", "-A", "JAFAR_NAT_OUTPUT", "-p", "tcp",
 		"--dport", "443", "-m", "owner", "!", "--uid-owner", "0",
 		"-j", "DNAT", "--to", address,
 	)
@@ -93,22 +93,22 @@ func (s *linuxShell) hijackHTTP(address string) error {
 	// We need to whitelist root otherwise the traffic sent by Jafar
 	// itself will match the rule and loop.
 	return shellx.Run(
-		"iptables", "-t", "nat", "-A", "JAFAR_NAT_OUTPUT", "-p", "tcp",
+		"sudo", "iptables", "-t", "nat", "-A", "JAFAR_NAT_OUTPUT", "-p", "tcp",
 		"--dport", "80", "-m", "owner", "!", "--uid-owner", "0",
 		"-j", "DNAT", "--to", address,
 	)
 }
 
 func (s *linuxShell) waive() error {
-	shellx.Run("iptables", "-D", "OUTPUT", "-j", "JAFAR_OUTPUT")
-	shellx.Run("iptables", "-D", "INPUT", "-j", "JAFAR_INPUT")
-	shellx.Run("iptables", "-t", "nat", "-D", "OUTPUT", "-j", "JAFAR_NAT_OUTPUT")
-	shellx.Run("iptables", "-F", "JAFAR_INPUT")
-	shellx.Run("iptables", "-X", "JAFAR_INPUT")
-	shellx.Run("iptables", "-F", "JAFAR_OUTPUT")
-	shellx.Run("iptables", "-X", "JAFAR_OUTPUT")
-	shellx.Run("iptables", "-t", "nat", "-F", "JAFAR_NAT_OUTPUT")
-	shellx.Run("iptables", "-t", "nat", "-X", "JAFAR_NAT_OUTPUT")
+	shellx.Run("sudo", "iptables", "-D", "OUTPUT", "-j", "JAFAR_OUTPUT")
+	shellx.Run("sudo", "iptables", "-D", "INPUT", "-j", "JAFAR_INPUT")
+	shellx.Run("sudo", "iptables", "-t", "nat", "-D", "OUTPUT", "-j", "JAFAR_NAT_OUTPUT")
+	shellx.Run("sudo", "iptables", "-F", "JAFAR_INPUT")
+	shellx.Run("sudo", "iptables", "-X", "JAFAR_INPUT")
+	shellx.Run("sudo", "iptables", "-F", "JAFAR_OUTPUT")
+	shellx.Run("sudo", "iptables", "-X", "JAFAR_OUTPUT")
+	shellx.Run("sudo", "iptables", "-t", "nat", "-F", "JAFAR_NAT_OUTPUT")
+	shellx.Run("sudo", "iptables", "-t", "nat", "-X", "JAFAR_NAT_OUTPUT")
 	return nil
 }
 
