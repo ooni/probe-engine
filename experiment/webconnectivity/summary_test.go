790d2458850f9858931ab5bcc40679a295261e25
Simone Basso <bassosimone@gmail.com>
2020-08-14 15:57:24 +0200

    QA/webconnectivity: timeout error in redirect chain (#850)
    Again, likely doing the redirect chain is not necessary. But it makes
    things more interesting, because we are testing stuff that happens after
    the initial DNS, TCP, etc. checks performed by Web Connectivity.
    
    Part of https://github.com/ooni/probe-engine/issues/810

--- experiment/webconnectivity/summary_test.go
+++ experiment/webconnectivity/summary_test.go
@@ -223,8 +223,8 @@ func TestSummarize(t *testing.T) {
 			},
 		},
 		wantOut: webconnectivity.Summary{
-			BlockingReason: &tcpIP,
-			Blocking:       &tcpIP,
+			BlockingReason: &httpFailure,
+			Blocking:       &httpFailure,
 			Accessible:     &falseValue,
 		},
 	}, {
