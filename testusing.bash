667e532a14f0bd446162b755a30a82ac11359a1b
Simone Basso <bassosimone@gmail.com>
2020-08-26 13:45:04 +0200

    github/workflows: simulate a user integrating probe-engine
    We want to have some confidence that a user can setup a Go repository
    and use probe-engine as a library. While this seems simple, our current
    dependency structure is complicated by the fact that psiphon does not
    have a go.mod file, so sometimes there are breakages.
    
    Part of https://github.com/ooni/probe-engine/issues/746

---
+++ testusing.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#
+# This script simulates a user creating a new project that depends
+# on github.com/ooni/probe-engine@GITHUB_SHA.
+#
+
+set -ex
+mkdir -p example.org/x
+cd example.org/x
+go mod init example.org/x
+echo > main.go << EOF
+package main
+
+import "github.com/ooni/probe-engine/libminiooni
+
+func main() {
+    libminiooni.Main()
+}
+EOF
+go get -v github.com/ooni/probe-engine@GITHUB_SHA
+go build -v .
+./x -OTunnel=psiphon -ni https://www.example.com urlgetter
