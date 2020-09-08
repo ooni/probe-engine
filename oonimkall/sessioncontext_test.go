58ae82fa381cda0b8dd2ef8e57f8fb1d5ba18673
Simone Basso <bassosimone@gmail.com>
2020-09-08 23:34:22 +0200

    fix data race

--- oonimkall/sessioncontext_test.go
+++ oonimkall/sessioncontext_test.go
@@ -1,9 +1,10 @@
 package oonimkall
 
 import (
-	"sync/atomic"
 	"testing"
 	"time"
+
+	"github.com/ooni/probe-engine/atomicx"
 )
 
 func TestClampTimeout(t *testing.T) {
@@ -25,77 +26,77 @@ func TestClampTimeout(t *testing.T) {
 }
 
 func TestNewContextWithZeroTimeout(t *testing.T) {
-	var here uint32
+	here := atomicx.NewInt64()
 	ctx, cancel := newContext(0)
 	defer cancel()
 	go func() {
 		<-time.After(250 * time.Millisecond)
 		cancel()
-		atomic.AddUint32(&here, 1)
+		here.Add(1)
 	}()
 	<-ctx.Done()
-	if here != 1 {
+	if here.Load() != 1 {
 		t.Fatal("context timeout not working as intended")
 	}
 }
 
 func TestNewContextWithNegativeTimeout(t *testing.T) {
-	var here uint32
+	here := atomicx.NewInt64()
 	ctx, cancel := newContext(-1)
 	defer cancel()
 	go func() {
 		<-time.After(250 * time.Millisecond)
 		cancel()
-		atomic.AddUint32(&here, 1)
+		here.Add(1)
 	}()
 	<-ctx.Done()
-	if here != 1 {
+	if here.Load() != 1 {
 		t.Fatal("context timeout not working as intended")
 	}
 }
 
 func TestNewContextWithHugeTimeout(t *testing.T) {
-	var here uint32
+	here := atomicx.NewInt64()
 	ctx, cancel := newContext(maxTimeout + 1)
 	defer cancel()
 	go func() {
 		<-time.After(250 * time.Millisecond)
 		cancel()
-		atomic.AddUint32(&here, 1)
+		here.Add(1)
 	}()
 	<-ctx.Done()
-	if here != 1 {
+	if here.Load() != 1 {
 		t.Fatal("context timeout not working as intended")
 	}
 }
 
 func TestNewContextWithReasonableTimeout(t *testing.T) {
-	var here uint32
+	here := atomicx.NewInt64()
 	ctx, cancel := newContext(1)
 	defer cancel()
 	go func() {
 		<-time.After(5 * time.Second)
 		cancel()
-		atomic.AddUint32(&here, 1)
+		here.Add(1)
 	}()
 	<-ctx.Done()
-	if here != 0 {
+	if here.Load() != 0 {
 		t.Fatal("context timeout not working as intended")
 	}
 }
 
 func TestNewContextWithArtificiallyLowMaxTimeout(t *testing.T) {
-	var here uint32
+	here := atomicx.NewInt64()
 	const maxTimeout = 2
 	ctx, cancel := newContextEx(maxTimeout+1, maxTimeout)
 	defer cancel()
 	go func() {
 		<-time.After(30 * time.Second)
 		cancel()
-		atomic.AddUint32(&here, 1)
+		here.Add(1)
 	}()
 	<-ctx.Done()
-	if here != 0 {
+	if here.Load() != 0 {
 		t.Fatal("context timeout not working as intended")
 	}
 }
