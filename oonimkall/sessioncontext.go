c572cf11106fc27186b3a6a2c016d0760c5b9396
Simone Basso <bassosimone@gmail.com>
2020-09-08 23:07:16 +0200

    better tests

--- oonimkall/sessioncontext.go
+++ oonimkall/sessioncontext.go
@@ -8,11 +8,20 @@ import (
 
 const maxTimeout = int64(time.Duration(math.MaxInt64) / time.Second)
 
+func clampTimeout(timeout, max int64) int64 {
+	if timeout > max {
+		timeout = max
+	}
+	return timeout
+}
+
 func newContext(timeout int64) (context.Context, context.CancelFunc) {
+	return newContextEx(timeout, maxTimeout)
+}
+
+func newContextEx(timeout, max int64) (context.Context, context.CancelFunc) {
 	if timeout > 0 {
-		if timeout > maxTimeout {
-			timeout = maxTimeout
-		}
+		timeout = clampTimeout(timeout, max)
 		return context.WithTimeout(
 			context.Background(), time.Duration(timeout)*time.Second)
 	}
--- oonimkall/sessioncontext_test.go
+++ oonimkall/sessioncontext_test.go
@@ -6,6 +6,24 @@ import (
 	"time"
 )
 
+func TestClampTimeout(t *testing.T) {
+	if clampTimeout(-1, maxTimeout) != -1 {
+		t.Fatal("unexpected result here")
+	}
+	if clampTimeout(0, maxTimeout) != 0 {
+		t.Fatal("unexpected result here")
+	}
+	if clampTimeout(60, maxTimeout) != 60 {
+		t.Fatal("unexpected result here")
+	}
+	if clampTimeout(maxTimeout, maxTimeout) != maxTimeout {
+		t.Fatal("unexpected result here")
+	}
+	if clampTimeout(maxTimeout+1, maxTimeout) != maxTimeout {
+		t.Fatal("unexpected result here")
+	}
+}
+
 func TestNewContextWithZeroTimeout(t *testing.T) {
 	var here uint32
 	ctx, cancel := newContext(0)
@@ -57,6 +75,22 @@ func TestNewContextWithReasonableTimeout(t *testing.T) {
 	defer cancel()
 	go func() {
 		<-time.After(5 * time.Second)
+		cancel()
+		atomic.AddUint32(&here, 1)
+	}()
+	<-ctx.Done()
+	if here != 0 {
+		t.Fatal("context timeout not working as intended")
+	}
+}
+
+func TestNewContextWithArtificiallyLowMaxTimeout(t *testing.T) {
+	var here uint32
+	const maxTimeout = 2
+	ctx, cancel := newContextEx(maxTimeout+1, maxTimeout)
+	defer cancel()
+	go func() {
+		<-time.After(30 * time.Second)
 		cancel()
 		atomic.AddUint32(&here, 1)
 	}()
