31ccadd68999896be23c7559c6da9d8e9e3b433c
xhdix <xhdix@yahoo.com>
2020-09-12 00:03:08 +0300

    Ensuring we can handle IDNs without issues
    Further study:
    https://developer.mozilla.org/en-US/docs/Mozilla/Internationalized_domain_names_support_in_Mozilla
    https://docs.microsoft.com/en-us/windows/win32/intl/handling-internationalized-domain-names--idns

--- netx/resolver/integration_test.go
+++ netx/resolver/integration_test.go
@@ -37,41 +37,72 @@ func testresolverquick(t *testing.T, reso resolver.Resolver) {
 	}
 }
 
+// Ensuring we can handle Internationalized Domain Names (IDNs) without issues
+func testresolverquickutf8(t *testing.T, reso resolver.Resolver) {
+	if testing.Short() {
+		t.Skip("skipping integration test in short mode")
+	}
+	reso = resolver.LoggingResolver{Logger: log.Log, Resolver: reso}
+	addrs, err := reso.LookupHost(context.Background(), "яндекс.рф")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if addrs == nil {
+		t.Fatal("expected non-nil addrs here")
+	}
+}
+
 func TestIntegrationNewResolverSystem(t *testing.T) {
-	testresolverquick(t, resolver.SystemResolver{})
+	reso := resolver.SystemResolver{}
+	testresolverquick(t, reso)
+	testresolverquickutf8(t, reso)
 }
 
 func TestIntegrationNewResolverUDPAddress(t *testing.T) {
-	testresolverquick(t, resolver.NewSerialResolver(
+	reso := resolver.NewSerialResolver(
 		resolver.NewDNSOverUDP(new(net.Dialer), "8.8.8.8:53")))
+	testresolverquick(t, reso)
+	testresolverquickutf8(t, reso)
 }
 
 func TestIntegrationNewResolverUDPDomain(t *testing.T) {
-	testresolverquick(t, resolver.NewSerialResolver(
-		resolver.NewDNSOverUDP(new(net.Dialer), "dns.google.com:53")))
+	reso := resolver.NewSerialResolver(
+		resolver.NewDNSOverUDP(new(net.Dialer), "dns.google.com:53"))
+	testresolverquick(t, reso)
+	testresolverquickutf8(t, reso)
 }
 
 func TestIntegrationNewResolverTCPAddress(t *testing.T) {
-	testresolverquick(t, resolver.NewSerialResolver(
-		resolver.NewDNSOverTCP(new(net.Dialer).DialContext, "8.8.8.8:53")))
+	reso := resolver.NewSerialResolver(
+		resolver.NewDNSOverTCP(new(net.Dialer).DialContext, "8.8.8.8:53"))
+	testresolverquick(t, reso)
+	testresolverquickutf8(t, reso)
 }
 
 func TestIntegrationNewResolverTCPDomain(t *testing.T) {
-	testresolverquick(t, resolver.NewSerialResolver(
-		resolver.NewDNSOverTCP(new(net.Dialer).DialContext, "dns.google.com:53")))
+	reso := resolver.NewSerialResolver(
+		resolver.NewDNSOverTCP(new(net.Dialer).DialContext, "dns.google.com:53"))
+	testresolverquick(t, reso)
+	testresolverquickutf8(t, reso)
 }
 
 func TestIntegrationNewResolverDoTAddress(t *testing.T) {
-	testresolverquick(t, resolver.NewSerialResolver(
-		resolver.NewDNSOverTLS(resolver.DialTLSContext, "8.8.8.8:853")))
+	reso := resolver.NewSerialResolver(
+		resolver.NewDNSOverTLS(resolver.DialTLSContext, "8.8.8.8:853"))
+	testresolverquick(t, reso)
+	testresolverquickutf8(t, reso)
 }
 
 func TestIntegrationNewResolverDoTDomain(t *testing.T) {
-	testresolverquick(t, resolver.NewSerialResolver(
-		resolver.NewDNSOverTLS(resolver.DialTLSContext, "dns.google.com:853")))
+	reso := resolver.NewSerialResolver(
+		resolver.NewDNSOverTLS(resolver.DialTLSContext, "dns.google.com:853"))
+	testresolverquick(t, reso)
+	testresolverquickutf8(t, reso)
 }
 
 func TestIntegrationNewResolverDoH(t *testing.T) {
-	testresolverquick(t, resolver.NewSerialResolver(
-		resolver.NewDNSOverHTTPS(http.DefaultClient, "https://doh.powerdns.org/")))
+	reso := resolver.NewSerialResolver(
+		resolver.NewDNSOverHTTPS(http.DefaultClient, "https://doh.powerdns.org/"))
+	testresolverquick(t, reso)
+	testresolverquickutf8(t, reso))
 }
