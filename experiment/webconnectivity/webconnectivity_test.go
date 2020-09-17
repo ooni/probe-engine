86264d8f806ac896a7d11f3c0653f2ea03bccccb
Simone Basso <bassosimone@gmail.com>
2020-09-17 12:40:23 +0200

    all: use ams-pg.ooni.org as testing probe service (#942)
    There are two reasons for this change:
    
    1. we wanna make sure ams-pg.ooni.org passes all tests
    
    2. mia-ps-test.ooni.io is currently broken
    
    See https://github.com/ooni/backend/issues/446

--- experiment/webconnectivity/webconnectivity_test.go
+++ experiment/webconnectivity/webconnectivity_test.go
@@ -196,7 +196,7 @@ func newsession(t *testing.T, lookupBackends bool) model.ExperimentSession {
 	sess, err := engine.NewSession(engine.SessionConfig{
 		AssetsDir: "../../testdata",
 		AvailableProbeServices: []model.Service{{
-			Address: "https://ps-test.ooni.io",
+			Address: "https://ams-pg.ooni.org",
 			Type:    "https",
 		}},
 		Logger: log.Log,
