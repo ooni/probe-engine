84c87ff64a6b46905c5c9ed39fc16934b9fa3579
cyBerta <cyBerta@users.noreply.github.com>
2020-09-21 16:15:56 +0200

    workaround to build probe-engine with go1.15 (#944)
    * workaround to build probe-engine with go1.15 (#866)
    
    * change order of arguments so that empty -tags argument will be ignored correctly

---
+++ readme_compiletimecheck.go
@@ -0,0 +1,8 @@
+//+build !DISABLE_QUIC
+//+build go1.15
+
+package engine
+
+
+ATTENTION: If you are compiling probe-engine with go1.15 please make sure
+to pass -tags DISABLE_QUIC. Alternatively use the build script!
