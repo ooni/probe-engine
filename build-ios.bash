cf2bed39277d2447395b005a5037d30605f7f723
Simone Basso <bassosimone@gmail.com>
2020-10-07 12:37:24 +0200

    build-ios.bash: remove workaround required by ObjectiveC++ (#973)
    Part of https://github.com/ooni/probe-engine/issues/668

--- build-ios.bash
+++ build-ios.bash
@@ -9,8 +9,3 @@ gomobile init
 export GO111MODULE=on
 output=MOBILE/ios/oonimkall.framework
 gomobile bind -target=ios -o $output -ldflags="-s -w" ./oonimkall
-# See https://github.com/ooni/probe-engine/issues/668
-for header in $output/Headers/*.objc.h; do
-  cat $header | sed 's|^@import Foundation;|#import <Foundation/Foundation.h>|g' > $header.new
-  mv $header.new $header
-done
