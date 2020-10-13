3953b850e4db4bdb3a16cab9174ec9944bf61a9b
xhdix <xhdix@yahoo.com>
2020-10-13 18:27:06 +0300

    fix(libminiooni): more robust file-opening code

---
+++ internal/fsx/fsx.go
@@ -0,0 +1,48 @@
+// Package fsx contains file system extension
+package fsx
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+)
+
+type File interface {
+	Stat() (os.FileInfo, error)
+	Read([]byte) (int, error)
+	Close() error
+}
+
+type FS interface {
+	Open(name string) (File, error)
+}
+
+// Open is a wrapper for os.Open
+func Open(pathname string) (File, error) {
+	return OpenWithFS(filesystem{}, pathname)
+}
+
+// OpenWithFS is to ensure that we're not attempting to open a directory.
+func OpenWithFS(fs FS, pathname string) (File, error) {
+	file, err := fs.Open(pathname)
+	if err != nil {
+		return nil, err
+	}
+	info, err := file.Stat()
+	if err != nil {
+		file.Close()
+		return nil, err
+	}
+	if info.IsDir() {
+		file.Close()
+		return nil, fmt.Errorf("input path points to a directory: %w", syscall.EISDIR)
+	}
+	return file, nil
+}
+
+type filesystem struct{}
+
+// Open with filesystem uses os.Open directly
+func (filesystem) Open(pathname string) (File, error) {
+	return os.Open(pathname)
+}
