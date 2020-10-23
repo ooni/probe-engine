49ea12763314f571edf905306dd7f60fe2790476
Simone Basso <bassosimone@gmail.com>
2020-10-23 14:41:52 +0200

    chore: update the user-agent header we use
    See https://github.com/ooni/probe-engine/issues/972

--- internal/httpheader/useragent.go
+++ internal/httpheader/useragent.go
@@ -3,7 +3,7 @@ package httpheader
 
 // UserAgent returns the User-Agent header used for measuring.
 func UserAgent() string {
-	// 8.9% as of Aug 21, 2020 according to https://techblog.willshouse.com/2012/01/03/most-common-user-agents/
-	const ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
+	// 13.1% as of Oct 23, 2020 according to https://techblog.willshouse.com/2012/01/03/most-common-user-agents/
+	const ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
 	return ua
 }
