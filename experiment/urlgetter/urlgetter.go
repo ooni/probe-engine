d60a13b33120388acd1a4aacca9c4319a1980187
Simone Basso <bassosimone@gmail.com>
2020-11-04 14:49:44 +0100

    make change very obvious

--- experiment/urlgetter/urlgetter.go
+++ experiment/urlgetter/urlgetter.go
@@ -19,22 +19,25 @@ const (
 
 // Config contains the experiment's configuration.
 type Config struct {
-	CertPool          *x509.CertPool // not settable from the command line
-	DNSCache          string         `ooni:"Add 'DOMAIN IP...' to cache"`
-	DNSHTTPHost       string         `ooni:"Force using specific HTTP Host header for DNS requests"`
-	DNSTLSServerName  string         `ooni:"Force TLS to using a specific SNI for encrypted DNS requests"`
-	FailOnHTTPError   bool           `ooni:"Fail HTTP request if status code is 400 or above"`
-	HTTP3Enabled      bool           `ooni:"Force http3"`
-	HTTPHost          string         `ooni:"Force using specific HTTP Host header"`
-	Method            string         `ooni:"Force HTTP method different than GET"`
-	NoFollowRedirects bool           `ooni:"Disable following redirects"`
-	NoTLSVerify       bool           `ooni:"Disable TLS verification"`
-	RejectDNSBogons   bool           `ooni:"Fail DNS lookup if response contains bogons"`
-	ResolverURL       string         `ooni:"URL describing the resolver to use"`
-	TLSServerName     string         `ooni:"Force TLS to using a specific SNI in Client Hello"`
-	TLSVersion        string         `ooni:"Force specific TLS version (e.g. 'TLSv1.3')"`
-	Tunnel            string         `ooni:"Run experiment over a tunnel, e.g. psiphon"`
-	UserAgent         string         `ooni:"Use the specified User-Agent"`
+	// not settable from command line
+	CertPool *x509.CertPool
+
+	// settable from command line
+	DNSCache          string `ooni:"Add 'DOMAIN IP...' to cache"`
+	DNSHTTPHost       string `ooni:"Force using specific HTTP Host header for DNS requests"`
+	DNSTLSServerName  string `ooni:"Force TLS to using a specific SNI for encrypted DNS requests"`
+	FailOnHTTPError   bool   `ooni:"Fail HTTP request if status code is 400 or above"`
+	HTTP3Enabled      bool   `ooni:"Force http3"`
+	HTTPHost          string `ooni:"Force using specific HTTP Host header"`
+	Method            string `ooni:"Force HTTP method different than GET"`
+	NoFollowRedirects bool   `ooni:"Disable following redirects"`
+	NoTLSVerify       bool   `ooni:"Disable TLS verification"`
+	RejectDNSBogons   bool   `ooni:"Fail DNS lookup if response contains bogons"`
+	ResolverURL       string `ooni:"URL describing the resolver to use"`
+	TLSServerName     string `ooni:"Force TLS to using a specific SNI in Client Hello"`
+	TLSVersion        string `ooni:"Force specific TLS version (e.g. 'TLSv1.3')"`
+	Tunnel            string `ooni:"Run experiment over a tunnel, e.g. psiphon"`
+	UserAgent         string `ooni:"Use the specified User-Agent"`
 }
 
 // TestKeys contains the experiment's result.
