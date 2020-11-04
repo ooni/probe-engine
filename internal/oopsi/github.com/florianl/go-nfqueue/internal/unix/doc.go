a8b0a4abd43caa7a0b14e731d744b2306a8bdd40
Simone Basso <bassosimone@gmail.com>
2020-11-04 08:53:27 +0100

    use an external fork

--- internal/oopsi/github.com/florianl/go-nfqueue/internal/unix/doc.go
+++
@@ -1,3 +0,0 @@
-// Package unix maps constants from github.com/ooni/probe-engine/internal/oopsi/golang.org/x/sys/unix to local constant
-// and makes them available for other platforms as well.
-package unix
--- internal/oopsi/github.com/redjack/marionette/plugins/plugins.go
+++
@@ -1,9 +0,0 @@
-package plugins
-
-import (
-	_ "github.com/ooni/probe-engine/internal/oopsi/github.com/redjack/marionette/plugins/channel"
-	_ "github.com/ooni/probe-engine/internal/oopsi/github.com/redjack/marionette/plugins/fte"
-	_ "github.com/ooni/probe-engine/internal/oopsi/github.com/redjack/marionette/plugins/io"
-	_ "github.com/ooni/probe-engine/internal/oopsi/github.com/redjack/marionette/plugins/model"
-	_ "github.com/ooni/probe-engine/internal/oopsi/github.com/redjack/marionette/plugins/tg"
-)
