c1b4d6fcff558f1e319b80a21b97a5d0f40ba3a8
Simone Basso <bassosimone@gmail.com>
2020-11-11 10:24:59 +0100

    feature(tlstool): very rough tool to experiment with SNI splitting (#1035)
    First exploratory step towards https://github.com/ooni/probe-engine/issues/622.

---
+++ experiment/tlstool/internal/split.go
@@ -0,0 +1,54 @@
+package internal
+
+import (
+	"bytes"
+	"context"
+	"net"
+	"time"
+
+	"github.com/ooni/probe-engine/netx"
+)
+
+// SplitDialer is a dialer that splits writes according to a
+// pattern and may delay the second write of delay milliseconds.
+type SplitDialer struct {
+	netx.Dialer
+	Delay   int64
+	Pattern string
+}
+
+// DialContext implements netx.Dialer.DialContext.
+func (d SplitDialer) DialContext(ctx context.Context, network, address string) (net.Conn, error) {
+	conn, err := d.Dialer.DialContext(ctx, network, address)
+	if err != nil {
+		return nil, err
+	}
+	return SplitConn{Conn: conn, Delay: d.Delay, Pattern: []byte(d.Pattern)}, nil
+}
+
+// SplitConn is a conn that splits writes.
+type SplitConn struct {
+	net.Conn
+	BeforeSecondWrite func() // for testing
+	Delay             int64
+	Pattern           []byte
+}
+
+// Write implements net.Conn.Write.
+func (c SplitConn) Write(b []byte) (int, error) {
+	if idx := bytes.Index(b, c.Pattern); idx > -1 {
+		idx += len(c.Pattern) / 2
+		if _, err := c.Conn.Write(b[:idx]); err != nil {
+			return 0, err
+		}
+		<-time.After(time.Duration(c.Delay) * time.Millisecond)
+		if c.BeforeSecondWrite != nil {
+			c.BeforeSecondWrite()
+		}
+		if _, err := c.Conn.Write(b[idx:]); err != nil {
+			return 0, err
+		}
+		return len(b), nil
+	}
+	return c.Conn.Write(b)
+}
