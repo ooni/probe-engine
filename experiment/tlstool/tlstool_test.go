be566923470285387b04328abfc067392104e6eb
Simone Basso <bassosimone@gmail.com>
2020-11-12 17:24:32 +0100

    fix: use correct version number

--- experiment/tlstool/tlstool_test.go
+++ experiment/tlstool/tlstool_test.go
@@ -15,7 +15,7 @@ func TestMeasurerExperimentNameVersion(t *testing.T) {
 	if measurer.ExperimentName() != "tlstool" {
 		t.Fatal("unexpected ExperimentName")
 	}
-	if measurer.ExperimentVersion() != "0.0.2" {
+	if measurer.ExperimentVersion() != "0.0.3" {
 		t.Fatal("unexpected ExperimentVersion")
 	}
 }
