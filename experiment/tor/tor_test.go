3f00359a5245ab391241ec5d2c557d92c40f4258
Simone Basso <bassosimone@gmail.com>
2020-11-25 12:21:16 +0100

    feat(tor): continue introducing api required by probe-cli (#1074)
    Work is part of https://github.com/ooni/probe/issues/1283.

--- experiment/tor/tor_test.go
+++ experiment/tor/tor_test.go
@@ -26,7 +26,7 @@ func TestNewExperimentMeasurer(t *testing.T) {
 	if measurer.ExperimentName() != "tor" {
 		t.Fatal("unexpected name")
 	}
-	if measurer.ExperimentVersion() != "0.2.0" {
+	if measurer.ExperimentVersion() != "0.3.0" {
 		t.Fatal("unexpected version")
 	}
 }
@@ -862,4 +862,69 @@ func TestMaybeScrubbingLogger(t *testing.T) {
 			t.Fatal("not the output type we expected")
 		}
 	})
+}
+
+func TestSummaryKeysInvalidType(t *testing.T) {
+	measurement := new(model.Measurement)
+	m := &Measurer{}
+	_, err := m.GetSummaryKeys(measurement)
+	if err.Error() != "invalid test keys type" {
+		t.Fatal("not the error we expected")
+	}
+}
+
+func TestSummaryKeysWorksAsIntended(t *testing.T) {
+	tests := []struct {
+		tk        TestKeys
+		isAnomaly bool
+	}{{
+		tk:        TestKeys{},
+		isAnomaly: false,
+	}, {
+		tk:        TestKeys{DirPortAccessible: 1, DirPortTotal: 3},
+		isAnomaly: false,
+	}, {
+		tk:        TestKeys{DirPortAccessible: 0, DirPortTotal: 3},
+		isAnomaly: true,
+	}, {
+		tk:        TestKeys{OBFS4Accessible: 1, OBFS4Total: 3},
+		isAnomaly: false,
+	}, {
+		tk:        TestKeys{OBFS4Accessible: 0, OBFS4Total: 3},
+		isAnomaly: true,
+	}, {
+		tk:        TestKeys{ORPortDirauthAccessible: 1, ORPortDirauthTotal: 3},
+		isAnomaly: false,
+	}, {
+		tk:        TestKeys{ORPortDirauthAccessible: 0, ORPortDirauthTotal: 3},
+		isAnomaly: true,
+	}, {
+		tk:        TestKeys{ORPortAccessible: 1, ORPortTotal: 3},
+		isAnomaly: false,
+	}, {
+		tk:        TestKeys{ORPortAccessible: 0, ORPortTotal: 3},
+		isAnomaly: true,
+	}}
+	for idx, tt := range tests {
+		t.Run(fmt.Sprintf("%d", idx), func(t *testing.T) {
+			m := &Measurer{}
+			measurement := &model.Measurement{TestKeys: &tt.tk}
+			got, err := m.GetSummaryKeys(measurement)
+			if err != nil {
+				t.Fatal(err)
+				return
+			}
+			sk := got.(SummaryKeys)
+			if sk.IsAnomaly != tt.isAnomaly {
+				t.Fatal("unexpected isAnomaly value")
+			}
+		})
+	}
+}
+
+func TestLogSummary(t *testing.T) {
+	m := &Measurer{}
+	if err := m.LogSummary(log.Log, "xyz"); err != nil {
+		t.Fatal(err)
+	}
 }
