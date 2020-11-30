36d40ced30036a3e1fa26ff0e9407ff6a1b8a579
kelmenhorst <kelmenhorst@uni-osnabrueck.de>
2020-11-30 12:07:47 +0100

    h3 qa: added tests for custom SNI and CA bundle

--- netx/httptransport/http3transport_test.go
+++ netx/httptransport/http3transport_test.go
@@ -3,6 +3,7 @@ package httptransport_test
 import (
 	"context"
 	"crypto/tls"
+	"crypto/x509"
 	"errors"
 	"net/http"
 	"strings"
@@ -18,6 +19,76 @@ type MockHTTP3Dialer struct{}
 
 func (d MockHTTP3Dialer) Dial(network, host string, tlsCfg *tls.Config, cfg *quic.Config) (quic.EarlySession, error) {
 	return quic.DialAddrEarly(host, tlsCfg, cfg)
+}
+
+type MockSNIHTTP3Dialer struct {
+	namech chan string
+}
+
+func (d MockSNIHTTP3Dialer) Dial(network, host string, tlsCfg *tls.Config, cfg *quic.Config) (quic.EarlySession, error) {
+	d.namech <- tlsCfg.ServerName
+	close(d.namech)
+	return quic.DialAddrEarly(host, tlsCfg, cfg)
+}
+
+type MockCertHTTP3Dialer struct {
+	certch chan *x509.CertPool
+}
+
+func (d MockCertHTTP3Dialer) Dial(network, host string, tlsCfg *tls.Config, cfg *quic.Config) (quic.EarlySession, error) {
+	d.certch <- tlsCfg.RootCAs
+	close(d.certch)
+	return quic.DialAddrEarly(host, tlsCfg, cfg)
+}
+
+func TestUnitHTTP3TransportSNI(t *testing.T) {
+	namech := make(chan string, 1)
+	sni := "sni.org"
+	txp := httptransport.NewHTTP3Transport(httptransport.Config{Dialer: selfcensor.SystemDialer{}, HTTP3Dialer: MockSNIHTTP3Dialer{namech: namech}, TLSDialer: dialer.TLSDialer{Config: &tls.Config{ServerName: sni}}})
+	req, err := http.NewRequest("GET", "https://www.google.com", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp, err := txp.RoundTrip(req)
+	if err == nil {
+		t.Fatal("expected error here")
+	}
+	if resp != nil {
+		t.Fatal("expected nil resp here")
+	}
+	if !strings.Contains(err.Error(), "certificate is valid for www.google.com, not "+sni) {
+		t.Fatal("unexpected error type", err)
+	}
+	servername := <-namech
+	if servername != sni {
+		t.Fatal("unexpected server name", servername)
+	}
+}
+
+func TestUnitHTTP3TransportCABundle(t *testing.T) {
+	certch := make(chan *x509.CertPool, 1)
+	certpool := x509.NewCertPool()
+	txp := httptransport.NewHTTP3Transport(httptransport.Config{Dialer: selfcensor.SystemDialer{}, HTTP3Dialer: MockCertHTTP3Dialer{certch: certch}, TLSDialer: dialer.TLSDialer{Config: &tls.Config{RootCAs: certpool}}})
+	req, err := http.NewRequest("GET", "https://www.google.com", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp, err := txp.RoundTrip(req)
+	if err == nil {
+		t.Fatal("expected error here")
+	}
+	if resp != nil {
+		t.Fatal("expected nil resp here")
+	}
+	// since the certificate pool is empty, the unknown authority error should be thrown
+	if !strings.Contains(err.Error(), "certificate signed by unknown authority") {
+		t.Fatal("unexpected error type")
+	}
+	certs := <-certch
+	if certs != certpool {
+		t.Fatal("not the certpool we expected")
+	}
+
 }
 
 func TestUnitHTTP3TransportSuccess(t *testing.T) {
