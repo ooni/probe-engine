bbeb03fe60f01d9a7bdc768ae1fa4804fe363780
Simone Basso <bassosimone@gmail.com>
2020-12-10 20:24:26 +0100

    feat(dnscheck): record experiment settings (#1134)
    This is still part of https://github.com/ooni/probe-engine/issues/1115

--- experiment/dnscheck/dnscheck_test.go
+++ experiment/dnscheck/dnscheck_test.go
@@ -48,7 +48,7 @@ func TestExperimentNameAndVersion(t *testing.T) {
 	if measurer.ExperimentName() != "dnscheck" {
 		t.Error("unexpected experiment name")
 	}
-	if measurer.ExperimentVersion() != "0.3.0" {
+	if measurer.ExperimentVersion() != "0.4.0" {
 		t.Error("unexpected experiment version")
 	}
 }
