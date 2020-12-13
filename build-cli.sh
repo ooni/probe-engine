f13dcba397205e0db6400efd8bd43ea592ae45d4
xhdix <xhdix@yahoo.com>
2020-12-14 01:14:26 +0300

    build automatically for rpi3 and rpi4

--- build-cli.sh
+++ build-cli.sh
@@ -10,6 +10,12 @@ case $1 in
   windows)
     export GOOS=windows GOARCH=amd64
     go build -o ./CLI/windows/amd64 -ldflags="-s -w" ./cmd/miniooni;;
+  rpi3)
+    export GOOS=linux GOARCH=arm GOARM=7
+    go build -o ./CLI/linux/armv7 -tags netgo -ldflags='-s -w -extldflags "-static"' ./cmd/miniooni;;
+  rpi4)
+    export GOOS=linux GOARCH=arm64
+    go build -o ./CLI/linux/arm64 -tags netgo -ldflags='-s -w -extldflags "-static"' ./cmd/miniooni;;
   *)
     echo "usage: $0 darwin|linux|windows" 1>&2
     exit 1
