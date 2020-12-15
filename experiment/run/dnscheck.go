dce20ad0a788e58f9431841438ac4552d920f7c9
Simone Basso <bassosimone@gmail.com>
2020-12-15 14:02:12 +0100

    fix(run): set dnscheck name and version
    Otherwise measurements are submitted as part of the run
    experiment, which is very unhelpful.
    
    See https://github.com/ooni/probe-engine/issues/1115.

--- experiment/run/dnscheck.go
+++ experiment/run/dnscheck.go
@@ -11,6 +11,8 @@ func dodnscheck(ctx context.Context, input StructuredInput,
 	sess model.ExperimentSession, measurement *model.Measurement,
 	callbacks model.ExperimentCallbacks) error {
 	exp := dnscheck.Measurer{Config: input.DNSCheck}
+	measurement.TestName = exp.ExperimentName()
+	measurement.TestVersion = exp.ExperimentVersion()
 	measurement.Input = model.MeasurementTarget(input.Input)
 	return exp.Run(ctx, sess, measurement, callbacks)
 }
