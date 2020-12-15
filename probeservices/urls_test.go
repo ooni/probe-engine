2a0b0256273cd9933dda8f7d108147f2483d09a7
Simone Basso <bassosimone@gmail.com>
2020-12-15 10:27:27 +0100

    feat: API for fetching/checking measurements (#1143)
    * feat: API for fetching/checking measurements
    
    See https://github.com/ooni/probe-engine/issues/948.
    
    Needed by mobile apps. It occurred to me this was "easy" to do
    while trying to close on https://github.com/ooni/e2etesting/pull/28.
    
    * Update cmd/apitool/main.go

--- probeservices/urls_test.go
+++ probeservices/urls_test.go
@@ -2,13 +2,10 @@ package probeservices_test
 
 import (
 	"context"
-	"net/http"
 	"strings"
 	"testing"
 
-	"github.com/apex/log"
 	"github.com/ooni/probe-engine/model"
-	"github.com/ooni/probe-engine/probeservices"
 )
 
 func TestFetchURLListSuccess(t *testing.T) {
@@ -49,45 +46,5 @@ func TestFetchURLListFailure(t *testing.T) {
 	}
 	if len(result) != 0 {
 		t.Fatal("results?!")
-	}
-}
-
-func TestURLsSuccess(t *testing.T) {
-	config := probeservices.URLsConfig{
-		BaseURL:           "https://ps1.ooni.io",
-		CountryCode:       "IT",
-		EnabledCategories: []string{"NEWS", "CULTR"},
-		HTTPClient:        http.DefaultClient,
-		Limit:             17,
-		Logger:            log.Log,
-		UserAgent:         "ooniprobe-engine/v0.1.0-dev",
-	}
-	ctx := context.Background()
-	result, err := probeservices.URLsQuery(ctx, config)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(result.Results) < 1 {
-		t.Fatal("no results")
-	}
-}
-
-func TestURLsFailure(t *testing.T) {
-	config := probeservices.URLsConfig{
-		BaseURL:           "\t\t\t",
-		CountryCode:       "IT",
-		EnabledCategories: []string{"NEWS", "CULTR"},
-		HTTPClient:        http.DefaultClient,
-		Limit:             17,
-		Logger:            log.Log,
-		UserAgent:         "ooniprobe-engine/v0.1.0-dev",
-	}
-	ctx := context.Background()
-	result, err := probeservices.URLsQuery(ctx, config)
-	if err == nil {
-		t.Fatal("expected an error here")
-	}
-	if result != nil {
-		t.Fatal("expected nil result here")
 	}
 }
