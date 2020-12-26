dae2993468ab0e72c0bfc53a80c60ab7d77c7afd
Simone Basso <bassosimone@gmail.com>
2020-12-26 14:24:22 +0100

    fix(build-cli.sh): say where binary is

--- build-cli.sh
+++ build-cli.sh
@@ -4,12 +4,18 @@ case $1 in
   darwin)
     export GOOS=darwin GOARCH=amd64
     go build -o ./CLI/darwin/amd64 -ldflags="-s -w" ./cmd/miniooni;;
+    echo ""
+    echo "Binary ready ad ./CLI/darwin/amd64/miniooni"
   linux)
     export GOOS=linux GOARCH=amd64
     go build -o ./CLI/linux/amd64 -tags netgo -ldflags='-s -w -extldflags "-static"' ./cmd/miniooni;;
+    echo ""
+    echo "Binary ready ad ./CLI/linux/amd64/miniooni"
   windows)
     export GOOS=windows GOARCH=amd64
     go build -o ./CLI/windows/amd64 -ldflags="-s -w" ./cmd/miniooni;;
+    echo ""
+    echo "Binary ready ad ./CLI/windows/amd64/miniooni.exe"
   *)
     echo "usage: $0 darwin|linux|windows" 1>&2
     exit 1
