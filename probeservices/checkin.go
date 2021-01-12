ad6dbcb22ea50da73efbc2a1f5503ed57585a895
Lorenzo Primiterra <lorenzo@openobservatory.org>
2021-01-12 15:01:26 +0000

    Update probeservices/checkin.go
    Co-authored-by: Simone Basso <bassosimone@gmail.com>

--- probeservices/checkin.go
+++ probeservices/checkin.go
@@ -14,7 +14,7 @@ type checkInResult struct {
 // CheckIn pobes ask for tests to be run or otherwise go back to sleep
 // The config argument contains the mandatory settings.
 // Returns the list of tests to run and the URLs, on success, or an explanatory error, in case of failure.
-func (c Client) CheckIn(ctx context.Context, config model.CheckInConfig) (model.CheckInInfo, error) {
+func (c Client) CheckIn(ctx context.Context, config model.CheckInConfig) (*model.CheckInInfo, error) {
 	var response checkInResult
 	if err := c.Client.PostJSON(ctx, "/api/v1/check-in", config, &response); err != nil {
 		return model.CheckInInfo{}, err
