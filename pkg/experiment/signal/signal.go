39c7d0897fa4caf2eefdd0eec0e0a7804a61f9ef
Simone Basso <bassosimone@gmail.com>
2023-06-04 16:46:15 +0200

    chore: sync with probe-cli v3.17.0-alpha.1-75-g9626fffd

--- pkg/experiment/signal/signal.go
+++ pkg/experiment/signal/signal.go
@@ -15,7 +15,7 @@ import (
 
 const (
 	testName    = "signal"
-	testVersion = "0.2.2"
+	testVersion = "0.2.3"
 
 	signalCA = `-----BEGIN CERTIFICATE-----
 MIID7zCCAtegAwIBAgIJAIm6LatK5PNiMA0GCSqGSIb3DQEBBQUAMIGNMQswCQYD
@@ -149,7 +149,7 @@ func (m Measurer) Run(ctx context.Context, args *model.ExperimentArgs) error {
 	defer cancel()
 	urlgetter.RegisterExtensions(measurement)
 
-	certPool := netxlite.NewDefaultCertPool()
+	certPool := netxlite.NewMozillaCertPool()
 	signalCAByteSlice := [][]byte{
 		[]byte(signalCA),
 		[]byte(signalCANew),
